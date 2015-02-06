816cc17b7e5f50cb643934be52ef954d26229a91
mertam <merta.m@gmail.com>
2015-02-06 08:31:15 +0100

    fixed test database setup

--- cz.mzk.recordmanager.server/src/test/resources/sql/recordmanager-insert-test.sql
+++ cz.mzk.recordmanager.server/src/test/resources/sql/recordmanager-insert-test.sql
@@ -1,4 +1,4 @@
 INSERT INTO library (id, name, url, catalog_url, city) VALUES (100, 'MZK', 'www.mzk.cz', 'vufind.mzk.cz', 'Brno');
 INSERT INTO contact_person (id, library_id, name, email, phone) VALUES (200, 100, 'Jan Novak', 'jan.novak@neexistuje.cz', '728 123 456');
 INSERT INTO oai_harvest_conf (id, library_id, url, set_spec, metadata_prefix, contact_person_id) VALUES (300, 100, 'http://aleph.mzk.cz/OAI', NULL, 'marc21', 200);
-INSERT INTO oai_harvest_conf (id, library_id, url, set_spec, granularity, metadata_prefix, contact_person_id) VALUES (301, 100, 'http://oai.medvik.cz/medvik2cpk/oai', NULL, 'YYYY-MM-DD', 'marc21', 200);
+INSERT INTO oai_harvest_conf (id, library_id, url, set_spec, granularity, metadata_prefix, contact_person_id) VALUES (301, 100, 'http://oai.medvik.cz/medvik2cpk/oai', NULL, 'DAY', 'marc21', 200);
