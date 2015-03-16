53f06945bf7d21e7db66374df338580c5f099e91
mertam <merta.m@gmail.com>
2015-03-16 14:20:34 +0100

    added simple DedupRecordMatcher test

--- cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/dedup/HarvestedRecordDedupMatcherTest.java
+++ cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/dedup/HarvestedRecordDedupMatcherTest.java
@@ -1,17 +1,34 @@
 package cz.mzk.recordmanager.server.dedup;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.testng.Assert;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import cz.mzk.recordmanager.server.AbstractTest;
+import cz.mzk.recordmanager.server.model.HarvestedRecord;
+import cz.mzk.recordmanager.server.oai.dao.HarvestedRecordDAO;
 
 public class HarvestedRecordDedupMatcherTest extends AbstractTest {
 	
 	@Autowired
 	HarvestedRecordDedupMatcher recordMatcher;
 	
+	@Autowired
+	HarvestedRecordDAO harvestedRecordDao;
+	
+	@BeforeMethod
+	public void before() throws Exception {
+		dbUnitHelper.init("dbunit/DedupRecordMatcherTest.xml");
+	}
+	
 	@Test
-	public void testMatcher() {
-//		TODO complete matcher tests
+	public void simpleMatcherTest() {
+		HarvestedRecord rec1 = harvestedRecordDao.get(100L);
+		HarvestedRecord rec2 = harvestedRecordDao.get(101L);
+		Assert.assertNotNull(rec1);
+		Assert.assertNotNull(rec2);
+		Assert.assertTrue(recordMatcher.matchRecords(rec1, rec1), "Same records not matched");
+		Assert.assertTrue(recordMatcher.matchRecords(rec1, rec2), "Matching failed");
 	}
 }
