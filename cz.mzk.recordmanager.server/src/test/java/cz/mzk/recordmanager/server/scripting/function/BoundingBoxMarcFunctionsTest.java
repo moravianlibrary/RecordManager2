fb04be7c506574557e833426aa310da1b7ff7e39
Vaclav Rosecky <xrosecky@gmail.com>
2015-05-04 20:09:42 +0200

    improved tests for BoundingBoxMarcFunctions

--- cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/scripting/function/BoundingBoxMarcFunctionsTest.java
+++ cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/scripting/function/BoundingBoxMarcFunctionsTest.java
@@ -17,12 +17,21 @@ public class BoundingBoxMarcFunctionsTest extends AbstractTest {
 	private BoundingBoxMarcFunctions functions;
 
 	@Test
-	public void parseRangesSingleYear() throws Exception {
+	public void testPrague() throws Exception {
 		List<String> data = new ArrayList<String>();
 		data.add("034 $aa $b20000 $dE0141016 $eE0144724 $fN0501302 $gN0495545");
 		MarcRecord record = MarcRecordFactory.recordFactory(data);
 		String result = functions.getBoundingBox(record );
 		Assert.assertEquals(result, "14.171111111111111 49.92916666666667 14.79 50.217222222222226");
+	}
+	
+	@Test // |a a  |b 6000000  |d E1023922  |e E1784848  |f N0023252  |g S0474141 
+	public void testAustralia() throws Exception {
+		List<String> data = new ArrayList<String>();
+		data.add("034 $aa $b6000000 $dE1023922  $eE1784848 $fN0023252 $gS0474141");
+		MarcRecord record = MarcRecordFactory.recordFactory(data);
+		String result = functions.getBoundingBox(record );
+		Assert.assertEquals(result, "102.65611111111112 -47.69472222222222 178.81333333333333 2.5477777777777777");
 	}
 
 }
