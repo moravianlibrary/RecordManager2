de80d71f9ed08e46810a4c4660cae49d65b63279
mertam <merta.m@gmail.com>
2015-06-23 09:54:38 +0200

    deduplication steps expanded to all formats

--- cz.mzk.recordmanager.server/src/main/resources/job/dedupRecordsJob/prepareTempAuthKeyTable.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/dedupRecordsJob/prepareTempAuthKeyTable.sql
@@ -1,17 +1,16 @@
 DROP TABLE IF EXISTS tmp_auth_keys;
 
 CREATE TABLE tmp_auth_keys AS
-	SELECT 
-	     title,
-	     array_to_string(array_agg(harvested_record.id), ',') as id_array,
-		count(*) AS total_count,
-		count(harvested_record.dedup_record_id) as dedup_count 
-	FROM harvested_record
-				INNER JOIN title ON title.harvested_record_id = harvested_record.id
-				INNER JOIN harvested_record_format_link ON harvested_record_format_link.harvested_record_id = harvested_record.id
-				INNER JOIN harvested_record_format ON harvested_record_format_link.harvested_record_format_id = harvested_record_format.id
-	WHERE harvested_record_format_link.harvested_record_format_id = 1
-	GROUP BY publication_year,title,author_auth_key
-	HAVING 
-		count(harvested_record.id) > 1 
-		AND count(harvested_record.id) > count(harvested_record.dedup_record_id);
+SELECT 
+  title,
+  hrl.harvested_record_format_id,
+  array_to_string(array_agg(hr.id), ',') as id_array,
+  count(*) AS total_count,
+  count(hr.dedup_record_id) as dedup_count 
+FROM harvested_record hr
+  INNER JOIN title t ON t.harvested_record_id = hr.id
+  INNER JOIN harvested_record_format_link hrl ON hrl.harvested_record_id = hr.id
+GROUP BY t.title,hr.publication_year,hr.author_auth_key,hrl.harvested_record_format_id
+HAVING count(hr.id) > 1 AND count(hr.id) > count(hr.dedup_record_id);
+
+CREATE INDEX tmp_auth_keys_idx ON tmp_auth_keys(id_array);
--- cz.mzk.recordmanager.server/src/main/resources/job/dedupRecordsJob/prepareTempCnbTable.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/dedupRecordsJob/prepareTempCnbTable.sql
@@ -1,25 +1,20 @@
 DROP TABLE IF EXISTS tmp_simmilar_books_cnb;
 
 CREATE TABLE tmp_simmilar_books_cnb AS
-	SELECT 
-		cnb.cnb,
-		title.title,
-		harvested_record.publication_year,
-		array_to_string(array_agg(harvested_record.id), ',') as id_array,
-		count(harvested_record.id) as total_count,
-		count(harvested_record.dedup_record_id) as dedup_count  
-		FROM harvested_record
-			INNER JOIN cnb on cnb.harvested_record_id = harvested_record.id 
-			INNER JOIN title on title.harvested_record_id = harvested_record.id
-			INNER JOIN harvested_record_format_link on harvested_record_format_link.harvested_record_id = harvested_record.id
-			INNER JOIN harvested_record_format on harvested_record_format_link.harvested_record_format_id = harvested_record_format.id
-		WHERE
-			harvested_record_format.name = 'BOOKS' AND
-			title.order_in_record = 1 
-		GROUP BY cnb.cnb,title.title,harvested_record.publication_year
-		HAVING 
-			count(harvested_record.id) > 1 AND
-		 	count(harvested_record.id) > count(harvested_record.dedup_record_id)
-		ORDER BY total_count DESC;
+SELECT
+  c.cnb,
+  t.title,
+  hr.publication_year,
+  hrl.harvested_record_format_id,
+  array_to_string(array_agg(hr.id), ',')  id_array,
+  count(*) total_count,
+  count(hr.dedup_record_id) dedup_count
+FROM harvested_record hr 
+  INNER JOIN cnb c ON hr.id = c.harvested_record_id 
+  INNER JOIN title t ON hr.id = t.harvested_record_id
+  INNER JOIN harvested_record_format_link hrl on hr.id = hrl.harvested_record_id
+WHERE t.order_in_record = 1
+GROUP BY c.cnb,t.title,hr.publication_year,hrl.harvested_record_format_id
+HAVING count(hr.id) > 1 AND count(hr.id) > count(hr.dedup_record_id);
 		
 CREATE INDEX tmp_cbn_idx ON tmp_simmilar_books_cnb(id_array);
