9b552676e658d26046cb216757b58e1d32644e6b
Vaclav Rosecky <xrosecky@gmail.com>
2015-06-25 21:02:52 +0200

    SQL updated for harvested_record_view

--- cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-create-views-postgres.sql
+++ cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-create-views-postgres.sql
@@ -1,11 +1,9 @@
 CREATE VIEW harvested_record_view AS
 SELECT
-  oai_harvest_conf_id,
-  oai_record_id,
+  import_conf_id,
+  record_id,
   deleted,
   format,
-  isbn,
-  title,
   convert_from(raw_record, 'UTF8') AS raw_record
 FROM
   harvested_record
