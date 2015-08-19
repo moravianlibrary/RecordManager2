be66f8173cfe7297a0ddb7a43866321c52b53b86
tomascejpek <tomas.cejpek@email.cz>
2015-08-19 07:56:06 +0200

    updated availability

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/scripting/marc/function/GetStatusesMarcFunctions.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/scripting/marc/function/GetStatusesMarcFunctions.java
@@ -21,6 +21,11 @@ public class GetStatusesMarcFunctions implements MarcRecordFunctions {
 	protected final static String ONLINE = "online";
 	protected final static Pattern ONLINE_PATTERN = Pattern.compile("(?i).*"+ONLINE+".*");
 	
+	protected final static String UNKNOWN = "nz";
+	protected final static String UNSPECIFIED = "n";
+	protected final static String LIMITED = "o";
+	protected final static String TEMPORARY = "d";
+
 	public List<String> getStatuses(MarcRecord record) {
 		List<String> statuses = new ArrayList<String>();
 		statuses.addAll(getStatuses(record, "996"));
@@ -45,6 +50,10 @@ public class GetStatusesMarcFunctions implements MarcRecordFunctions {
 		List<String> statuses = new ArrayList<String>();
 		boolean present = false;
 		boolean absent = false;
+		boolean unknown = false;
+		boolean unspecified = false;
+		boolean limited = false;
+		boolean temporary = false;
 		for (DataField field : fields) {
 			Subfield s = field.getSubfield('s');
 			if (s == null) {
@@ -58,6 +67,18 @@ public class GetStatusesMarcFunctions implements MarcRecordFunctions {
 			case PRESENT:
 				present = true;
 				break;
+			case UNKNOWN:
+				unknown = true;
+				break;
+			case UNSPECIFIED:
+				unspecified = true;
+				break;
+			case LIMITED:
+				limited = true;
+				break;
+			case TEMPORARY:
+				temporary = true;
+				break;
 			}
 		}
 		if (absent) {
@@ -65,6 +86,18 @@ public class GetStatusesMarcFunctions implements MarcRecordFunctions {
 		}
 		if (present) {
 			statuses.add("present");
+		}
+		if (unknown) {
+			statuses.add("unknown");
+		}
+		if (unspecified) {
+			statuses.add("unspecified");
+		}
+		if (limited) {
+			statuses.add("limited");
+		}
+		if (temporary) {
+			statuses.add("temporary");
 		}
 		return statuses;
 	}
