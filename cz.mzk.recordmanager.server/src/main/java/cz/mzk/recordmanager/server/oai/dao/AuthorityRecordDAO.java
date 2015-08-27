aa409158903f55b31b9c4e27f5174914abda26b4
mertam <merta.m@gmail.com>
2015-08-27 10:45:29 +0200

    removed obsolete method

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/oai/dao/AuthorityRecordDAO.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/oai/dao/AuthorityRecordDAO.java
@@ -1,13 +1,9 @@
 package cz.mzk.recordmanager.server.oai.dao;
 
-import java.util.List;
-
 import cz.mzk.recordmanager.server.model.AuthorityRecord;
-import cz.mzk.recordmanager.server.model.DedupRecord;
 import cz.mzk.recordmanager.server.model.ImportConfiguration;
 
 public interface AuthorityRecordDAO extends DomainDAO<Long, AuthorityRecord> {
 	public AuthorityRecord findByIdAndHarvestConfiguration(String recordId, ImportConfiguration configuration);
-	public List<AuthorityRecord> findByDedupRecord(DedupRecord dedupRecord);
 	public AuthorityRecord findByAuthKey(String AuthKey);
 }
