efc48e2ab7239d4dcac7c5cb55cc771fc6fdc2be
mertam <merta.m@gmail.com>
2015-10-07 13:55:01 +0200

    fixed database creation scripts

--- cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-create-tables-derby.sql
+++ cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-create-tables-derby.sql
@@ -200,17 +200,16 @@ CREATE TABLE antikvariaty_catids (
   CONSTRAINT antikvariaty_catids_fk FOREIGN KEY (antikvariaty_id) REFERENCES antikvariaty(id)
 );
 
-
 CREATE TABLE fulltext_monography (
-  id			DECIMAL(10) PRIMARY KEY,
-  harvested_record_id	DECIMAL(10),
-  uuid_page		VARCHAR(42),
-  is_private		BOOLEAN DEFAULT TRUE,
-  order_in_monography	DECIMAL(10),
-  page 			VARCHAR(20),
-  fulltext		BLOB,
+  id                  DECIMAL(10) PRIMARY KEY,
+  harvested_record_id DECIMAL(10),
+  uuid_page           VARCHAR(42),
+  is_private          BOOLEAN, 
+  order_in_monography DECIMAL(10),
+  page                VARCHAR(20),
+  fulltext            BLOB,
   CONSTRAINT fulltext_monography_harvested_record_id_fk FOREIGN KEY (harvested_record_id) REFERENCES harvested_record(id)
-);
+); 
 
 CREATE TABLE skat_keys (
   skat_record_id      DECIMAL(10),
