0261788b2316039355540281325792ff0d45155c
Vaclav Rosecky <xrosecky@gmail.com>
2015-11-09 20:35:18 +0100

    logging in KrameriusFulltexterSolr

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/kramerius/fulltext/KrameriusFulltexterSolr.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/kramerius/fulltext/KrameriusFulltexterSolr.java
@@ -11,6 +11,8 @@ import org.apache.solr.client.solrj.request.QueryRequest;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.SolrDocumentList;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Charsets;
 
@@ -20,6 +22,9 @@ import cz.mzk.recordmanager.server.solr.SolrServerFacade;
 import cz.mzk.recordmanager.server.util.SolrUtils;
 
 public class KrameriusFulltexterSolr implements KrameriusFulltexter {
+
+	private static Logger logger = LoggerFactory
+			.getLogger(KrameriusFulltexterSolr.class);
 
 	private static final String FL_FIELDS = String.join(",", UUID_FIELD, FULLTEXT_FIELD, PAGE_NUMBER_FIELD, PAGE_ORDER_FIELD, PID_FIELD);
 
@@ -35,6 +40,7 @@ public class KrameriusFulltexterSolr implements KrameriusFulltexter {
 	@Override
 	public List<FulltextMonography> getFulltextObjects(String rootUuid)
 			throws IOException {
+		logger.debug("About to harvest fulltext from Kramerius for {}", rootUuid);
 		SolrQuery query = new SolrQuery();
 		String queryString = SolrUtils.createEscapedFieldQuery(PARENT_PID_FIELD, rootUuid) + " AND " +
 				SolrUtils.createEscapedFieldQuery(FEDORA_MODEL_FIELD, FEDORA_MODEL_PAGE);
