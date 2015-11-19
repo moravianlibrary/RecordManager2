b014b3c13d011cc00771991ad052b7c746e60a71
Milan Janicek <mjtecka@gmail.com>
2015-11-19 17:28:45 +0100

    Locally set Locale doesn't affect getBoundingBoxAsPolygon method output anymore (Locale.US is always used)

--- cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/scripting/function/BoundingBoxMarcFunctionsTest.java
+++ cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/scripting/function/BoundingBoxMarcFunctionsTest.java
@@ -1,5 +1,6 @@
 package cz.mzk.recordmanager.server.scripting.function;
 
+import java.text.MessageFormat;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -50,7 +51,6 @@ public class BoundingBoxMarcFunctionsTest extends AbstractTest {
 		data.add("034 $aa $b200000 $dE0115843 $eE0185426 $fN0510158 $gN0483256");
 		MarcRecord record = MarcRecordFactory.recordFactory(data);
 		String result = functions.getBoundingBoxAsPolygon(record);
-		System.out.println(result);
 		Assert.assertEquals(result, "POLYGON((11.979 48.549, 18.907 48.549, 18.907 51.033, 11.979 51.033, 11.979 48.549))");
 	}
 
