982b5cf3bc6451ea71d195781294a4e5be8c0f08
mertam <merta.m@gmail.com>
2015-11-27 11:38:34 +0100

    do not persist unchanged HarvestedRecords during deduplication

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/dedup/DedupSimpleKeysStepWriter.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/dedup/DedupSimpleKeysStepWriter.java
@@ -2,6 +2,7 @@ package cz.mzk.recordmanager.server.dedup;
 
 import java.sql.SQLException;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -11,6 +12,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import cz.mzk.recordmanager.server.model.DedupRecord;
 import cz.mzk.recordmanager.server.model.HarvestedRecord;
 import cz.mzk.recordmanager.server.oai.dao.HarvestedRecordDAO;
 
@@ -38,12 +40,46 @@ public class DedupSimpleKeysStepWriter implements
 			throws Exception {
 		for (List<HarvestedRecord> hrList: arg0) {
 			for (HarvestedRecord hr: hrList) {
-				harvestedRecordDAO.persist(hr);
+				
+				// check whether is need to store current record
+				DedupRecord dr = hr.getDedupRecord();
+				if (dr != null) {
+					
+				}
+				
+				if (checkIfUpdateIsNeeded(hr)) {
+					harvestedRecordDAO.persist(hr);
+				}
+
 				totalCount++;
 				logProgress(false);
 			}
 		}
 
+	}
+	
+	/**
+	 * @param hr
+	 * @return False if harvested record was updated before it's dedupRecord updated time
+	 */
+	protected boolean checkIfUpdateIsNeeded(HarvestedRecord hr) {
+		
+		if (hr == null) {
+			return false;
+		}
+		
+		DedupRecord dr = hr.getDedupRecord();
+		if (dr != null && dr.getUpdated() == null) {
+			return true;
+		}
+		
+		Date drUpdate = dr.getUpdated();
+		
+		if (hr.getUpdated() != null) {
+			return drUpdate.compareTo(hr.getUpdated()) > 0;
+		}
+		
+		return true;
 	}
 	
 	protected void logProgress(boolean force) {
