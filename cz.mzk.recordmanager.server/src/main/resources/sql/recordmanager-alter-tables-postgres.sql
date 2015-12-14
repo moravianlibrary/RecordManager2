a2af13bd51a36a92d5a574a39aa9299e51d5f948
tomascejpek <tomas.cejpek@email.cz>
2015-12-14 14:38:46 +0100

    updated vktatest id_prefix

--- cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-alter-tables-postgres.sql
+++ cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-alter-tables-postgres.sql
@@ -340,3 +340,6 @@ UPDATE oai_harvest_conf SET url='http://opac.moderniknihovna.cz/cgi-bin/koha/oai
 
 -- 3. 12. 2015 tomascejpek
 UPDATE oai_harvest_conf SET set_spec='CPK1' WHERE import_conf_id=328;
+
+-- 14. 12. 2015 tomascejpek
+UPDATE import_conf SET id_prefix='vktatest' WHERE id=329;
