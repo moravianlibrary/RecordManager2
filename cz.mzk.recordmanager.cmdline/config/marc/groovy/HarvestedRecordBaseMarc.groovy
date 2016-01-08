14daa3e56141c8fa8c05c1a199959288abe6efea
Vaclav Rosecky <xrosecky@gmail.com>
2016-01-08 19:07:03 +0100

    field mapping fixed - hidden prefix

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/HarvestedRecordBaseMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/HarvestedRecordBaseMarc.groovy
@@ -21,8 +21,8 @@ local_statuses_facet_str_mv = getStatuses()
 
 holdings_996_str_mv = getHoldings996()
 
-authority_dummy_field = getFields "1007:7007"
-authors_dummy = getFields "100abcd:700abcd"
+_hidden_authority_dummy_field = getFields "1007:7007"
+_hidden_authors_dummy = getFields "100abcd:700abcd"
 sfx_links = getSfxIds()
 
 barcodes = getFields "996b"
