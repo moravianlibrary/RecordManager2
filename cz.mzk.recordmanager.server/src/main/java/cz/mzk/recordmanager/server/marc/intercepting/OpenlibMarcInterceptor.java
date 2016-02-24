26ddf984f121a977d14b2fa627b5d1a9a39b1e55
Tomáš Cejpek <tomas.cejpek@email.cz>
2016-02-24 13:54:05 +0100

    updated openlibrary links

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/marc/intercepting/OpenlibMarcInterceptor.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/marc/intercepting/OpenlibMarcInterceptor.java
@@ -3,11 +3,14 @@ package cz.mzk.recordmanager.server.marc.intercepting;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.marc4j.marc.ControlField;
 import org.marc4j.marc.DataField;
 import org.marc4j.marc.MarcFactory;
 import org.marc4j.marc.Record;
+import org.marc4j.marc.Subfield;
 
 import cz.mzk.recordmanager.server.export.IOFormat;
 import cz.mzk.recordmanager.server.marc.MarcRecord;
@@ -18,6 +21,8 @@ import cz.mzk.recordmanager.server.marc.marc4j.RecordImpl;
 public class OpenlibMarcInterceptor extends DefaultMarcInterceptor{
 
 	private static final String TEXT_856y = "Elektronická výpůjčka přes OpenLibrary (nutná osobní registrace)";
+	
+	private static final Pattern OPENLIBRARY_URL = Pattern.compile("http://www.openlibrary.org.*");
 	
 	public OpenlibMarcInterceptor(Record record) {
 		super(record);
@@ -42,15 +47,21 @@ public class OpenlibMarcInterceptor extends DefaultMarcInterceptor{
 		Map<String, List<DataField>> dfMap = marc.getAllFields();
 		for(String tag: new TreeSet<String>(dfMap.keySet())){ // sorted tags
 			for(DataField df: dfMap.get(tag)){
-				
 				if(df.getTag().equals("856")){
 					// kill field 85641
 					if(df.getIndicator1()=='4' && df.getIndicator2()=='1') continue;
 					if(df.getIndicator1()=='4' && df.getIndicator2()=='2'){
-						DataField newDf = marcFactory.newDataField("856", '4', '2');
-						newDf.addSubfield(df.getSubfield('u'));
-						newDf.addSubfield(marcFactory.newSubfield('y', TEXT_856y));
-						newRecord.addVariableField(newDf);
+						Subfield sf = df.getSubfield('u');
+						if(sf != null){
+							Matcher matcher = OPENLIBRARY_URL.matcher(sf.getData());
+							if(matcher.matches()){
+								DataField newDf = marcFactory.newDataField("856", '4', '2');
+								newDf.addSubfield(df.getSubfield('u'));
+								newDf.addSubfield(marcFactory.newSubfield('y', TEXT_856y));
+								newRecord.addVariableField(newDf);
+							}
+							else newRecord.addVariableField(df);
+						}
 					}
 				}
 				else newRecord.addVariableField(df);
