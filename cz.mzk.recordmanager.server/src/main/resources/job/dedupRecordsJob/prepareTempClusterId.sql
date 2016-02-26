7244d37b05762d334fbd3e88fa4a7fb058d48fe1
mertam <merta.m@gmail.com>
2016-02-26 08:17:13 +0100

    refactored cluster_id deduplication query

--- cz.mzk.recordmanager.server/src/main/resources/job/dedupRecordsJob/prepareTempClusterId.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/dedupRecordsJob/prepareTempClusterId.sql
@@ -1,12 +1,15 @@
 DROP TABLE IF EXISTS tmp_cluster_ids;
 
 CREATE TABLE tmp_cluster_ids AS
-SELECT 
-  nextval('tmp_table_id_seq') as row_id,
-  array_to_string(array_agg(harvested_record.id), ',') AS id_array 
-FROM harvested_record WHERE cluster_id IN 
-  (SELECT cluster_id FROM harvested_record WHERE dedup_record_id is NULL OR next_dedup_flag IS TRUE GROUP BY cluster_id) 
-GROUP BY cluster_id
-HAVING COUNT(harvested_record.id) > 1 AND bool_or(next_dedup_flag) IS TRUE;
+SELECT
+  nextval('tmp_table_id_seq') AS row_id,
+  array_to_string(array_agg(DISTINCT id), ',') AS id_array
+FROM (
+  SELECT hr.id,hr.cluster_id,hr.next_dedup_flag
+    FROM harvested_record hr 
+      INNER JOIN (SELECT cluster_id FROM harvested_record WHERE cluster_id IS NOT NULL) cli ON hr.cluster_id = cli.cluster_id
+) AS sub
+GROUP BY sub.cluster_id
+HAVING COUNT(sub.id) > 1 AND bool_or(sub.next_dedup_flag) IS TRUE;
   
 CREATE INDEX tmp_cluster_idx ON tmp_cluster_ids(row_id);
