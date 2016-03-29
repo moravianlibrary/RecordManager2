e761621cddb34525423fbffa477e145092406e2b
Vaclav Rosecky <xrosecky@gmail.com>
2016-03-29 07:21:45 +0200

    marc mapping scripts updated

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/BaseMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/BaseMarc.groovy
@@ -1,57 +1,46 @@
 import static cz.mzk.recordmanager.server.util.MarcCleaningUtils.*;
 
-lccn = getFirstField "010a"
-ctrlnum = getFirstField "035a"
-
 recordtype = "marc"
 allfields = getAllFields()
 fullrecord = getFullrecord()
 
 language = translate("mzk_language.map", getLanguages(), null)
-country_txt = translate("mzk_country.map", getCountry(), null)
+country_str = translate("mzk_country.map", getCountry(), null)
 
 author = getFirstField "100abcd"
 author_fuller = getFirstField "100q"
-author_letter = getFirstField "100a"
 author2 = getFields "110ab:111ab:700abcd:710ab:711ab"
-author2_role = getFields "700e:710e"
 author_additional = getFields "505r"
+author_sort_str = getAuthorForSorting()
 
 title = getFirstField "245abnp"
 title_sub = getFirstField "245b"
-title_short = getFirstField "245a"
+title_short = getFirstFieldTrim "245a"
 title_full = getFirstField "245abdefghijklmnopqrstuvwxyz0123456789"
-title_auth = getFirstField "245ab"
 title_alt = getFields "130adfgklnpst:240a:246a:730adfgklnpst:740a"
 title_old = getFields "780ast"
 title_new = getFields "785ast"
 title_sort = getSortableTitle()
+title_auto_str = getTitleDisplay()
 series = getFields "440ap:800abcdfpqt:830ap"
 series2 = getFields "490a"
 
 publisher = getPublisher()
-publishDate_display = getPublishDateDisplay()
 placeOfPublication_txt_mv = getFieldsTrim "260a:264a"
 publishDate = getPublishDate()
 publishDateSort = getPublishDateForSorting()
 
-physical = getFields "300abcefg:530abcd"
 dateSpan = getFields "362a"
-edition = getFirstField "250a"
 contents = getFields "505a:505t"
 
 isbn = getFields "020a"
 issn = getFields "022a:440x:490x:730x:776x:780x:785x"
 
 callnumber_str_mv = getFields "910b"
-
+     
 topic = getFields "600:610:630:650"
 genre = getFields "655"
 geographic = getFields "651"
-
-topic_facet = cleanFacets(getFields("600x:610x:611x:630x:648x:650a:650x:651x:655x"))
-genre_facet = getFields "600v:610v:611v:630v:648v:650v:651v:655a:655v"
-geographic_facet = getFields "600z:610z:611z:630z:648z:650z:651a:651z:655z"
 
 url = getFields "856u"
 
@@ -61,13 +50,11 @@ subjectKeywords_search_txt_mv = getFieldsUnique "072x:600abcdfgqklmprstxyz7:610a
 issnIsbnIsmn_search_str_mv = getISBNISSNISMN()
 sourceTitle_search_txt_mv = getFieldsUnique "773adtkxz9"
 callNumber_search_txt_mv = getFieldsUnique "910b:996ch"
-publisher_search_str_mv = getFieldsTrim "260b:264b:928a:978abcdg7"
-id001_search_str = getId001()
+publisher_search_txt_mv = getFieldsTrim "260b:264b:928a:978abcdg7"
 cnb_search_str = getFirstField "015az"
 
 ean_str_mv = getEAN()
-illustrated = isIllustrated()
-subject_facet_str_mv = getSubject "600abcdfglnpqstyz:610abcdfgklnpstyz:611abcdefgklnpqstyz:630adfgklnpstyz2:648a:650avxyz:651avxyz:653a:964abcdefg:967abc"
+subject_facet_str_mv = getSubject "600abcdfglnpqstyz:610abcdfgklnpstyz:611abcdefgklnpqstyz:630adfgklnpstyz2:648a:650avxyz:651avxyz:653a:964abcdefg:967ab"
 source_title_facet_str = getFirstField "773t"
 genre_facet_str = getFirstField "655avxyz"
 conspectus_facet_str_mv = getFields "072x"
@@ -82,9 +69,9 @@ bbox_geo_str = getBoundingBox()
 // no need for this field?
 //statuses = getStatuses()
 
-// deprecated
-title_display = getFirstField "245abnp"
-
 //published = getFirstField "260a"
 
 availability_id_str_mv = getFieldsUnique "996w"
+
+citation_record_type = getCitationRecordType();
+author_find = getAuthorFind();
