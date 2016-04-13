b14eb60bf496123debaafc8f975d0a36231a82cb
Tomáš Cejpek <tomas.cejpek@email.cz>
2016-04-13 14:01:39 +0200

    removed facets series, period

--- cz.mzk.recordmanager.server/src/main/resources/marc/groovy/BaseMarc.groovy
+++ cz.mzk.recordmanager.server/src/main/resources/marc/groovy/BaseMarc.groovy
@@ -59,9 +59,7 @@ source_title_facet_str = getFirstField "773t"
 genre_facet_str = getFirstField "655avxyz"
 conspectus_facet_str_mv = getFields "072x"
 publisher_str_mv = filter("publisher.txt", getPublisherStrMv())
-series_facet_str_mv = getFieldsTrim "440a:490a:800abcdflnpqstv:810abnpst:811acdenpqst:830anps"
 author_facet_str_mv = getFields "100abcdq:110abc:111acdegq:700abcdq:710abc:711acdegq:975abcdq:976abc:978abcdgq"
-period_facet_str_mv = getPeriod()
 
 bbox_geo = getBoundingBoxAsPolygon()
 bbox_geo_str = getBoundingBox()
