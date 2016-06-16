3d397677b41307bf73ae640203c7a702b3e984b8
Tomáš Cejpek <tomas.cejpek@email.cz>
2016-06-16 10:50:11 +0200

    renamed short_note_cs(en) to short_note_cs(en)_display

--- cz.mzk.recordmanager.server/src/main/resources/marc/groovy/AuthorityMarc.groovy
+++ cz.mzk.recordmanager.server/src/main/resources/marc/groovy/AuthorityMarc.groovy
@@ -4,6 +4,7 @@ fullrecord = getFullrecord()
 
 source = getFields "670a"
 heading = getFirstAuthAuthor "100"
+heading_display = getFirstAuthAuthor "100"
 use_for = getAuthAuthors "400"
 see_also = getFields "500abcd"
 scope_note = getFields "678a"
@@ -15,5 +16,5 @@ subjectKeywords_search_txt_mv = getFields "100abcd:400abcd:500abcd"
 people_search_txt_mv = getFields "100abcd:400abcd:500abcd"
 subject_facet_str_mv = getFields "100abcd"
 
-short_note_cs = translate("auth_short_note_cs.map", getId001(), null)
-short_note_en = translate("auth_short_note_en.map", getId001(), null)
+short_note_cs_display = translate("auth_short_note_cs.map", getId001(), null)
+short_note_en_display = translate("auth_short_note_en.map", getId001(), null)
