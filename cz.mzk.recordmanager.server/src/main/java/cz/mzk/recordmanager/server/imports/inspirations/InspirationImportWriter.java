b91e777a1fdcb491117c60c33683a55af8b6c478
Tomáš Cejpek <tomas.cejpek@email.cz>
2016-06-30 13:22:10 +0200

    logger for InspirationImportWriter

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/imports/inspirations/InspirationImportWriter.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/imports/inspirations/InspirationImportWriter.java
@@ -7,6 +7,8 @@ import java.util.Map.Entry;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +24,8 @@ import cz.mzk.recordmanager.server.oai.dao.InspirationDAO;
 @Component
 @StepScope
 public class InspirationImportWriter implements ItemWriter<Map<String, List<String>>> {
+	
+	private static Logger logger = LoggerFactory.getLogger(InspirationImportWriter.class);
 	
 	@Autowired
 	private ImportConfigurationDAO confDao;
@@ -39,27 +43,44 @@ public class InspirationImportWriter implements ItemWriter<Map<String, List<Stri
 	
 	private static final Pattern PATTERN_ID = Pattern.compile("([^\\.]*)\\.(.*)");
 	
+	private static final String TEXT_INFO = "Importing inspiration '%s' with %s records";
+	private static final String TEXT_ADD = "Added %s inspirations";
+	private static final String TEXT_EXISTS = "%s inspirations is already exists in db";
+	private static final String TEXT_RECORD_NOT_EXIST = "%s harvested records not found";
+	private static final String TEXT_DELETE = "Deleted %s inspirations";
+	private static final String TEXT_COMPLETE = "Importing inspiration %s is COMPLETED";
+	
 	@Override
 	public void write(List<? extends Map<String, List<String>>> items)
 			throws Exception {
 		for(Map<String, List<String>> map: items){
 			for (Entry<String, List<String>> entry : map.entrySet()){
 				String inspiration_name = entry.getKey();
+				logger.info(String.format(TEXT_INFO, inspiration_name, entry.getValue().size()));
 				// actual list of records with inspiration in db
 				hrWithInspiration = inspirationDao.fingHrByInspiraion(inspiration_name);
+				int added_ins = 0; // added inspiration records
+				int exists_ins = 0; // inspirations in db
+				int not_exists_rec = 0; // hr not in db
 			    for(String id: entry.getValue()){
 			    	Matcher matcher = PATTERN_ID.matcher(id);
 			    	if(matcher.matches()){
 			    		String id_prefix = matcher.group(1);
 			    		String record_id = matcher.group(2);
-			    		for(ImportConfiguration conf: confDao.findByIdPrefix(id_prefix)){
+			    		List<ImportConfiguration> confs = confDao.findByIdPrefix(id_prefix);
+			    		int counter = 0;
+			    		for(ImportConfiguration conf: confs){
 				    		if(conf == null) continue;
 				    		HarvestedRecord hr = hrDao.findByIdAndHarvestConfiguration(record_id, conf);
-				    		if(hr == null) continue;
+				    		if(hr == null){
+				    			if(++counter == confs.size()) ++not_exists_rec;
+				    			continue;
+				    		}
 				    		
 				    		if(hrWithInspiration.contains(hr)){
 				    			// inspiration is already in db
 				    			hrWithInspiration.remove(hr);
+				    			++exists_ins;
 				    		}
 				    		else{ // add inspiration to hr 
 				    			List<Inspiration> result = hr.getInspiration();
@@ -69,6 +90,7 @@ public class InspirationImportWriter implements ItemWriter<Map<String, List<Stri
 				    			hr.setInspiration(result);
 				    			hr.setUpdated(new Date());
 				    			hrDao.persist(hr);
+				    			++added_ins;
 				    		}
 			    		}
 			    	}
@@ -83,6 +105,11 @@ public class InspirationImportWriter implements ItemWriter<Map<String, List<Stri
 					hrDao.persist(hr);
 					inspirationDao.delete(delete);
 			    }
+			    logger.info(String.format(TEXT_EXISTS, exists_ins));
+			    logger.info(String.format(TEXT_ADD, added_ins));
+			    logger.info(String.format(TEXT_RECORD_NOT_EXIST, not_exists_rec));
+			    logger.info(String.format(TEXT_DELETE, hrWithInspiration.size()));
+			    logger.info(String.format(TEXT_COMPLETE, inspiration_name));
 			}
 		}
 	}
