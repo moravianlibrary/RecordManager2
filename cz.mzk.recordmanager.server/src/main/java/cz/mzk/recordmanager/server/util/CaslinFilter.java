074df956c61da2828da47c283a90cb84170f920e
Tomáš Cejpek <tomas.cejpek@email.cz>
2016-08-19 08:34:36 +0200

    updated caslinFilter, sigla in cache

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/util/CaslinFilter.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/util/CaslinFilter.java
@@ -1,8 +1,11 @@
 package cz.mzk.recordmanager.server.util;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
 
+import org.apache.commons.collections4.map.LRUMap;
 import org.marc4j.marc.DataField;
 import org.marc4j.marc.Subfield;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,14 +18,25 @@ public class CaslinFilter {
 
 	@Autowired
 	private SiglaDAO siglaDao;
+	
+	private final int LRU_CACHE_SIZE = 100;
+	
+	private final Map<String, Boolean> siglaCache = Collections.synchronizedMap(new LRUMap<String, Boolean>(LRU_CACHE_SIZE));
 
 	public List<DataField> filter(List<DataField> input){
 		List<DataField> result = new ArrayList<DataField>();
 		for(DataField df: input){
 			Subfield sf = df.getSubfield('e');
 			if(sf == null) continue;
-			if(siglaDao.findSiglaByName(sf.getData()).isEmpty()){
-				result.add(df);
+			if(siglaCache.containsKey(sf.getData())){
+				if(!siglaCache.get(sf.getData())) result.add(df);
+			}
+			else{
+				if(siglaDao.findSiglaByName(sf.getData()).isEmpty()){
+					result.add(df);
+					siglaCache.put(sf.getData(), false);
+				}
+				else siglaCache.put(sf.getData(), true);
 			}
 		}
 		return result;
