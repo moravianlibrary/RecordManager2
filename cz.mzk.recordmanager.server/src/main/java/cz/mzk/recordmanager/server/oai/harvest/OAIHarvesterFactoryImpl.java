0aa3b19b23ac83212197b43cc729239ed2af52b5
Vaclav Rosecky <xrosecky@gmail.com>
2016-09-22 08:23:46 +0200

    FaultTolerantProxy for more reliable harvesting

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/oai/harvest/OAIHarvesterFactoryImpl.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/oai/harvest/OAIHarvesterFactoryImpl.java
@@ -3,6 +3,7 @@ package cz.mzk.recordmanager.server.oai.harvest;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import cz.mzk.recordmanager.server.util.HttpClient;
+import cz.mzk.recordmanager.server.util.proxy.FaultTolerantProxy;
 
 public class OAIHarvesterFactoryImpl implements OAIHarvesterFactory {
 
@@ -11,7 +12,8 @@ public class OAIHarvesterFactoryImpl implements OAIHarvesterFactory {
 	
 	@Override
 	public OAIHarvester create(OAIHarvesterParams parameters) {
-		return new OAIHarvesterImpl(httpClient, parameters);
+		OAIHarvester harvester = new OAIHarvesterImpl(httpClient, parameters);
+		return FaultTolerantProxy.create(harvester, 3, 1000);
 	}
 
 }
