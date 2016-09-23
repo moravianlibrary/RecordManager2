52968a982fb609050bee4d22c5cc34339d6f785e
Vaclav Rosecky <xrosecky@gmail.com>
2016-09-23 08:43:09 +0200

    webapp: initial spring configuration

---
+++ cz.mzk.recordmanager.webapp/src/main/java/cz/mzk/recordmanager/webapp/WebAppConfig.java
@@ -0,0 +1,48 @@
+package cz.mzk.recordmanager.webapp;
+
+import java.beans.PropertyVetoException;
+import java.io.File;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import cz.mzk.recordmanager.server.ClasspathResourceProvider;
+import cz.mzk.recordmanager.server.DelegatingResourceProvider;
+import cz.mzk.recordmanager.server.FileSystemResourceProvider;
+import cz.mzk.recordmanager.server.ResourceProvider;
+
+@Configuration
+@PropertySource(value={"file:${CONFIG_DIR:.}/database.properties", "file:${CONFIG_DIR:.}/database.local.properties"}, ignoreResourceNotFound=true)
+public class WebAppConfig {
+
+	@Autowired
+	private Environment environment;
+
+	@Bean
+	public DataSource dataSource(@Value("${jdbc.driverClassName}") String driverClassName, @Value("${jdbc.url}") String url, 
+			@Value("${jdbc.username}") String username, @Value("${jdbc.password}") String password) throws PropertyVetoException {
+		ComboPooledDataSource dataSource = new ComboPooledDataSource();
+		dataSource.setDriverClass(driverClassName);
+		dataSource.setJdbcUrl(url);
+		dataSource.setUser(username);
+		dataSource.setPassword(password);
+		return dataSource;
+	}
+
+	@Bean
+	public ResourceProvider resourceProvider(@Value("${CONFIG_DIR}") File configDir) {
+		return new DelegatingResourceProvider( //
+				new FileSystemResourceProvider(configDir), //
+				new ClasspathResourceProvider() //
+		);
+	}
+
+}
