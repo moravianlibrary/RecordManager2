e72288fddb5d21bf621a41127ca60c2975216be3
SergeyPanov <panovsy@gmail.com>
2016-10-31 09:42:48 +0100

    Statistic spec file

---
+++ cz.mzk.recordmanager.webapp.gui/gui/src/app/model/oai-harvest-job-statistics.spec.ts
@@ -0,0 +1,10 @@
+/* tslint:disable:no-unused-variable */
+
+import { addProviders, async, inject } from '@angular/core/testing';
+import {OaiHarvestJobStatistics} from './oai-harvest-job-statistics';
+
+describe('OaiHarvestJobStatistics', () => {
+  it('should create an instance', () => {
+    expect(new OaiHarvestJobStatistics()).toBeTruthy();
+  });
+});
