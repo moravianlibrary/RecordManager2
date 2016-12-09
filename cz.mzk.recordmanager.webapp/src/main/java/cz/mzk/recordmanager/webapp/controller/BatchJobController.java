f5510c573420852b6d3f2c7b5918a6e5bba88d47
Vaclav Rosecky <xrosecky@gmail.com>
2016-12-09 08:21:20 +0100

    coding style fixed

--- cz.mzk.recordmanager.webapp/src/main/java/cz/mzk/recordmanager/webapp/controller/BatchJobController.java
+++ cz.mzk.recordmanager.webapp/src/main/java/cz/mzk/recordmanager/webapp/controller/BatchJobController.java
@@ -1,16 +1,12 @@
 package cz.mzk.recordmanager.webapp.controller;
 
 import cz.mzk.recordmanager.api.model.IdDto;
-import cz.mzk.recordmanager.api.model.ImportRecordsDto;
 import cz.mzk.recordmanager.api.model.RecordIdDto;
 import cz.mzk.recordmanager.api.model.batch.BatchJobExecutionDTO;
 import cz.mzk.recordmanager.api.service.BatchService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.multipart.MultipartHttpServletRequest;
-
-import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -24,49 +20,46 @@ public class BatchJobController {
 	@Autowired
 	private BatchService batchService;
 
-
 	@RequestMapping(method = RequestMethod.GET, value = "/{jobId}")
 	@ResponseBody
-	private BatchJobExecutionDTO getJobExecution(@PathVariable Long jobId)
-	{
+	private BatchJobExecutionDTO getJobExecution(@PathVariable Long jobId) {
 		return batchService.getJobExecution(jobId);
 	}
 
 	@RequestMapping(method = RequestMethod.GET, value = "/running")
 	@ResponseBody
-	private List<BatchJobExecutionDTO> getRunningJobExecutions()
-	{
+	private List<BatchJobExecutionDTO> getRunningJobExecutions() {
 		return batchService.getRunningJobExecutions();
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/run/fullHarvest")
 	@ResponseBody
-	private void runFullHarvest(@RequestBody List<IdDto> configId){
+	private void runFullHarvest(@RequestBody List<IdDto> configId) {
 		batchService.runFullHarvest(configId);
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/run/incrementalHarvest")
 	@ResponseBody
-	private void runIncrementalHarvest(@RequestBody List<IdDto> configId){
+	private void runIncrementalHarvest(@RequestBody List<IdDto> configId) {
 		batchService.runIncrementalHarvest(configId);
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/run/deduplicate")
 	@ResponseBody
-	private void runDuduplicate(){
+	private void runDuduplicate() {
 		batchService.runDeduplicate();
 	}
 
 
 	@RequestMapping(method = RequestMethod.POST, value = "/run/index")
 	@ResponseBody
-	private void runIndex(){
+	private void runIndex() {
 		batchService.runIndex();
 	}
 
 	@RequestMapping(method = RequestMethod.POST, value = "/run/indexIndividualIndexesToSolr")
 	@ResponseBody
-	private void runIndexIndividual(@RequestBody List<RecordIdDto>  ids){
+	private void runIndexIndividual(@RequestBody List<RecordIdDto> ids) {
 		batchService.runIndividualIndex(ids);
 	}
 
@@ -90,21 +83,20 @@ public class BatchJobController {
 
 	@RequestMapping(method = RequestMethod.POST, value = "/run/filterCaslinRecordsJob")
 	@ResponseBody
-	private void runFilterCaslinRecordsJob(){
+	private void runFilterCaslinRecordsJob() {
 		batchService.runFilterCaslinRecordsJob();
 	}
 
-
 	@RequestMapping(method = RequestMethod.POST, value = "/run/regenerateDedupKeysJob")
 	@ResponseBody
-	private void runRegenerateDedupKeysJob(){
+	private void runRegenerateDedupKeysJob() {
 		batchService.runRegenerateDedupKeysJob();
 	}
 
-
 	@RequestMapping(method = RequestMethod.POST, value = "/run/regenerateMissingDedupKeysJob")
 	@ResponseBody
-	private void regenerateMissingDedupKeysJob(){
+	private void regenerateMissingDedupKeysJob() {
 		batchService.runRegenerateMissingDedupKeysJob();
 	}
+
 }
--- cz.mzk.recordmanager.webapp/src/main/java/cz/mzk/recordmanager/webapp/controller/OaiHarvestStatisticsController.java
+++ cz.mzk.recordmanager.webapp/src/main/java/cz/mzk/recordmanager/webapp/controller/OaiHarvestStatisticsController.java
@@ -8,22 +8,22 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-
 @RestController
 @RequestMapping(value = "/oaiHarvestStats")
 public class OaiHarvestStatisticsController {
-    @Autowired
-    private OaiHarvestStatisticsService oaiHarvestStatisticsService;
+	@Autowired
+	private OaiHarvestStatisticsService oaiHarvestStatisticsService;
 
-    @RequestMapping(method = RequestMethod.GET)
-    @ResponseBody
-    public List<OaiHarvestJobStatisticsDto> getStatistics() {
-        return oaiHarvestStatisticsService.getHarvestJobStatistics();
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseBody
+	public List<OaiHarvestJobStatisticsDto> getStatistics() {
+		return oaiHarvestStatisticsService.getHarvestJobStatistics();
+	}
 
-    @RequestMapping(method = RequestMethod.GET, value = "{configId}")
-    @ResponseBody
-    public IdDto getLibraryId(@PathVariable Long configId){
-        return oaiHarvestStatisticsService.getLibraryId(configId);
-    }
+	@RequestMapping(method = RequestMethod.GET, value = "{configId}")
+	@ResponseBody
+	public IdDto getLibraryId(@PathVariable Long configId) {
+		return oaiHarvestStatisticsService.getLibraryId(configId);
+	}
+
 }
