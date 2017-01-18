611b77f169c7299eed0346fc704e2c598a5f6eb6
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-01-18 14:29:48 +0100

    delete from ismn table before regeneration

--- cz.mzk.recordmanager.server/src/main/resources/job/regenerateDedupKeysJob/dropDedupKeys.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/regenerateDedupKeysJob/dropDedupKeys.sql
@@ -2,6 +2,7 @@ UPDATE harvested_record SET cluster_id = NULL, uuid = NULL, issn_series = NULL, 
 DELETE FROM title;
 DELETE FROM isbn;
 DELETE FROM issn;
+DELETE FROM ismn;
 DELETE FROM cnb;
 DELETE FROM language;
 DELETE FROM oclc;
