c99178dc1838489825740d06c3293cdcd1459131
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-02-16 12:39:52 +0100

    added citation_record_type_str for UNMZ

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/MzkNormsMetadataMarcRecord.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/MzkNormsMetadataMarcRecord.java
@@ -4,6 +4,7 @@ import java.util.Collections;
 import java.util.List;
 
 import cz.mzk.recordmanager.server.marc.MarcRecord;
+import cz.mzk.recordmanager.server.metadata.CitationRecordType;
 import cz.mzk.recordmanager.server.metadata.MetadataMarcRecord;
 import cz.mzk.recordmanager.server.model.HarvestedRecordFormat.HarvestedRecordFormatEnum;
 
@@ -16,6 +17,11 @@ public class MzkNormsMetadataMarcRecord extends MetadataMarcRecord{
 	@Override
 	public List<HarvestedRecordFormatEnum> getDetectedFormatList() {
 		return Collections.singletonList(HarvestedRecordFormatEnum.NORMS);		
+	}
+
+	@Override
+	public CitationRecordType getCitationFormat() {
+		return CitationRecordType.NORMS;
 	}
 
 }
