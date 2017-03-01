ee74765f2384826b87bb55cd28db5002e84eb6a7
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-03-01 13:04:14 +0100

    adresarKnihoven - added solr field projects_display_mv

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/AdresarKnihovenBaseMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/AdresarKnihovenBaseMarc.groovy
@@ -20,3 +20,4 @@ directory_display = getFirstFieldSeparatedForAdresar "ADKuz", " | "
 url_display_mv = getFieldsForAdresar "URLuz", SubfieldExtractionMethod.JOINED, " | "
 function_display_mv = translate("adresar_function.map", getFieldsForAdresar("FCEa", SubfieldExtractionMethod.SEPARATED, null), null)
 services_display_mv = translate("adresar_services.map", getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null), null)
+projects_display_mv = translate("adresar_projects.map", getFieldsForAdresar("PRKa", SubfieldExtractionMethod.SEPARATED, null), null)
