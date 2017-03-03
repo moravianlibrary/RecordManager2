00fbcdb6d5e035bf55a628e93065f0fd0824930d
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-03-03 13:17:11 +0100

    adresarKnihoven - added solr fields services_search/facet

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/AdresarKnihovenBaseMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/AdresarKnihovenBaseMarc.groovy
@@ -30,6 +30,7 @@ town_search_txt = getFirstFieldForAdresar "MESa"
 region_search_txt = getFirstFieldForAdresar "KRJa"
 district_search_txt = getFirstFieldForAdresar "KRJb"
 function_search_txt_mv = translate("adresar_function.map", getFieldsForAdresar("FCEa", SubfieldExtractionMethod.SEPARATED, null), null)
+services_search_txt_mv = translate("adresar_services.map", getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null), null)
 type_search_txt = getFirstFieldForAdresar "TYPb"
 note_search_txt = getFirstFieldForAdresar "POIa"
 code_search_txt = getFirstFieldForAdresar "ZKRa"
@@ -38,4 +39,5 @@ responsibility_search_txt_mv = getFieldsForAdresar "JMNkp", SubfieldExtractionMe
 
 region_disctrict_facet_str_mv = adresarGetRegionDistrictFacet()
 function_facet_str_mv = translate("adresar_function.map", getFieldsForAdresar("FCEa", SubfieldExtractionMethod.SEPARATED, null), null)
+services_facet_str_mv = translate("adresar_services.map", getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null), null)
 type_facet_str = getFirstFieldForAdresar "TYPb"
