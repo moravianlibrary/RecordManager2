3c2c71d116562e23143bfe12cd0af7122e1b4050
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-05-17 12:50:10 +0200

    updated AUDIO_CD format

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/MetadataMarcRecord.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/MetadataMarcRecord.java
@@ -601,7 +601,6 @@ public class MetadataMarcRecord implements MetadataRecord {
 		}
 		if(f300.matches("(?i).*zvukov([aáeé]|ych|ých)\\sdes(ka|ky|ek).*") && f300.matches("(?i).*(digital|12\\s*cm).*")) return HarvestedRecordFormatEnum.AUDIO_CD;
 		if (f300.matches("(?i).*audiodisk.*")) return HarvestedRecordFormatEnum.AUDIO_CD;
-		if (f338b.matches("(?i)sd")) return HarvestedRecordFormatEnum.AUDIO_CD;
 		
 		// AUDIO_LP
 		if(f300.matches("(?i).*gramofonov([aáeé]|ych|ých)\\sdes(ka|ky|ek).*")) return HarvestedRecordFormatEnum.AUDIO_LP;
