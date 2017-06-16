95e1f0a5bd6ec4fc716cfc086cd534cbd0fa6d67
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-06-16 12:55:50 +0200

    adresarKnihoven - update filter

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/LibraryMetadataMarcRecord.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/LibraryMetadataMarcRecord.java
@@ -1,9 +1,20 @@
 package cz.mzk.recordmanager.server.metadata.institutions;
 
+import java.util.regex.Pattern;
+
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.Subfield;
+
 import cz.mzk.recordmanager.server.marc.MarcRecord;
 import cz.mzk.recordmanager.server.metadata.MetadataMarcRecord;
 
 public class LibraryMetadataMarcRecord extends MetadataMarcRecord {
+
+	private static final Pattern LIBRARY_CLOSED = Pattern
+			.compile("KNIHOVNA ZRUŠENA!");
+	private static final Pattern INSTITUTION_CLOSED = Pattern
+			.compile("ZRUŠENÁ INSTITUCE!");
+
 	public LibraryMetadataMarcRecord(MarcRecord underlayingMarc) {
 		super(underlayingMarc);
 	}
@@ -13,9 +24,16 @@ public class LibraryMetadataMarcRecord extends MetadataMarcRecord {
 		if (underlayingMarc.getDataFields("DEL") != null
 				&& !underlayingMarc.getDataFields("DEL").isEmpty()) {
 			return false;
-
 		}
+		for (DataField df : underlayingMarc.getDataFields("STT")) {
+			Subfield sf;
+			if ((sf = df.getSubfield('a')) != null
+					&& (LIBRARY_CLOSED.matcher(sf.getData()).matches() || INSTITUTION_CLOSED
+							.matcher(sf.getData()).matches())) {
+				return false;
+			}
+		}
+
 		return true;
 	}
-
 }
