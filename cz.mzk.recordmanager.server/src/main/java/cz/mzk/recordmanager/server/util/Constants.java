f65fcf8c22dbdd3f97008d08117cbaf3f9c189ab
SergeyPanov <panovsy@gmail.com>
2017-07-13 13:41:52 +0200

    Added export for classifier job.

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/util/Constants.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/util/Constants.java
@@ -37,6 +37,7 @@ public class Constants {
 	public static final String JOB_ID_FULLTEXT_ZAKONYPROLIDI		= "zakonyProLidiFulltextJob";
 	public static final String JOB_ID_FILTER_CASLIN					= "filterCaslinRecordsJob";
 	public static final String JOB_ID_FULLTEXT_MANUSCRIPTORIUM		= "manuscriptoriumFulltextJob";
+	public static final String JOB_ID_EXPORT_RECORDS_FOR_CLASSIFIER = "exportRecordsForClassifierJob";
 	
 	public static final String JOB_PARAM_CONF_ID 		= "configurationId";
 	public static final String JOB_PARAM_FROM_DATE 		= "from";
