15dfbf33b37c68ce203bff0b6461bb461bd89adb
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-08-17 09:46:19 +0200

    sigla is available from importConfiguration

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/model/ImportConfiguration.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/model/ImportConfiguration.java
@@ -1,12 +1,19 @@
 package cz.mzk.recordmanager.server.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Inheritance;
 import javax.persistence.InheritanceType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
+
 import org.hibernate.annotations.Parameter;
 import org.hibernate.annotations.Type;
 
@@ -60,6 +67,10 @@ public abstract class ImportConfiguration extends AbstractDomainObject {
 
 	@Column(name="generate_dedup_keys")
 	private boolean generateDedupKeys = true;
+
+	@OneToMany(cascade = CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinColumn(name="import_conf_id", referencedColumnName="id")
+	private List<Sigla> siglas = new ArrayList<>();
 
 	public Library getLibrary() {
 		return library;
@@ -155,6 +166,14 @@ public abstract class ImportConfiguration extends AbstractDomainObject {
 
 	public void setMappingDedupScript(String mappingDedupScript) {
 		this.mappingDedupScript = mappingDedupScript;
+	}
+
+	public List<Sigla> getSiglas() {
+		return siglas;
+	}
+
+	public void setSiglas(List<Sigla> siglas) {
+		this.siglas = siglas;
 	}
 
 }
