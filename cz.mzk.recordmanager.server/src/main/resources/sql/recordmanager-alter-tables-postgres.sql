28f59f3f0d31b32e68d5e74b726f9467c65b7269
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-08-18 13:00:08 +0200

    update item_id configuration

--- cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-alter-tables-postgres.sql
+++ cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-alter-tables-postgres.sql
@@ -872,3 +872,10 @@ INSERT INTO download_import_conf (import_conf_id,url,import_job_name,format,extr
 
 -- 17. 08. 2017 tomascejpek
 ALTER TABLE import_conf ADD COLUMN item_id VARCHAR(15);
+
+-- 18. 08. 2017 tomascejpek
+UPDATE import_conf SET item_id='aleph',interception_enabled=true WHERE id in (300,304,307,313,315,321,324,325,326,330,335,337);
+UPDATE import_conf SET item_id='tre',interception_enabled=true WHERE id=306;
+UPDATE import_conf SET item_id='nlk',interception_enabled=true WHERE id=301;
+UPDATE import_conf SET item_id='svkul',interception_enabled=true WHERE id=314;
+UPDATE import_conf SET item_id='other',interception_enabled=true WHERE id in (302,308,311,312,328,334,336,338,340,343,346,350,353);
