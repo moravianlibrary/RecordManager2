a382146672956be0ae6d29a12fd4eb506810ea9d
Tomáš Cejpek <tomas.cejpek@email.cz>
2017-09-06 12:20:51 +0200

    add indexing scripts for dictionary

---
+++ cz.mzk.recordmanager.server/src/main/resources/marc/groovy/DictionaryLocal.groovy
@@ -0,0 +1,15 @@
+recordtype = "dictionary"
+
+fullrecord = getFullrecord()
+
+title = getFirstField "150a"
+
+english_display = getFirstField "750a"
+explanation_display = getFirstField "678a"
+relative_display_mv = getFields "550a"
+alternative_display_mv = getFields "450a"
+source_display = getFirstField "650a"
+format_display_mv = getFormat()
+
+subject_facet_str_mv = Arrays.asList("Knihovnictví", "Informační věda")
+local_institution_facet_str_mv = getInstitutionFacet()
---
+++ cz.mzk.recordmanager.server/src/main/resources/marc/groovy/DictionaryMerged.groovy
@@ -0,0 +1,5 @@
+recordtype = "marc"
+allfields = getAllFields()
+fullrecord = getFullrecord()
+
+cpk_detected_format_facet_str_mv = getFormat()
