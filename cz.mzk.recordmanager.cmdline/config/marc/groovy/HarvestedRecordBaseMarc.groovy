6932f820b8ed09637e3873ebc5f64115bdcde0fe
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-01-05 13:12:51 +0100

    solr field for countries with czech and english values

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/HarvestedRecordBaseMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/HarvestedRecordBaseMarc.groovy
@@ -1,3 +1,5 @@
+import org.apache.commons.collections4.ListUtils;
+
 recordtype = "local"
 fullrecord = getFullrecord()
 
@@ -47,3 +49,5 @@ format_display_mv = getFormat()
 local_institution_facet_str_mv = getInstitutionFacet()
 _hidden_index_when_merged_boolean = getIndexWhenMerged()
 item_id_txt_mv = getFirstField "996t"
+
+country_search_txt_mv = ListUtils.union(translate("mzk_country.map", getCountries(), null), translate("country_cs.map", getCountries(), null))
