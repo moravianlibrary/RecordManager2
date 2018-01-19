77e668f0519670c293ebb5bde01f5a3b095edc60
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-01-19 09:04:35 +0100

    add test for dublinCore export

--- cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/export/ExportRecordsJobTest.java
+++ cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/export/ExportRecordsJobTest.java
@@ -19,79 +19,91 @@ import cz.mzk.recordmanager.server.DBUnitHelper;
 import cz.mzk.recordmanager.server.util.Constants;
 
 public class ExportRecordsJobTest extends AbstractTest {
-	
+
 	@Autowired
 	private JobRegistry jobRegistry;
 
 	@Autowired
 	private JobLauncher jobLauncher;
-	
+
 	@Autowired
 	private DBUnitHelper dbUnitHelper;
-	
+
 	private static final String TEST_FILE_1 = "target/test/export1.txt";
 	private static final String TEST_FILE_2 = "target/test/export_iso2709.txt";
-    private static final String TEST_FILE_3 = "target/test/export_aleph.txt";
-    private static final String TEST_FILE_4 = "target/test/export.txt";
-	
+	private static final String TEST_FILE_3 = "target/test/export_aleph.txt";
+	private static final String TEST_FILE_4 = "target/test/export.txt";
+	private static final String TEST_FILE_5 = "target/test/exportDC.txt";
+
 	@BeforeMethod
 	public void init() throws Exception {
 		dbUnitHelper.init("dbunit/ExportRecords.xml");
 	}
-	
+
 	@BeforeClass
 	public void cleanUp() {
-		for (String filename : new String[]{ TEST_FILE_1, TEST_FILE_2, TEST_FILE_3, TEST_FILE_4 }) {
+		for (String filename : new String[]{TEST_FILE_1, TEST_FILE_2, TEST_FILE_3, TEST_FILE_4, TEST_FILE_5}) {
 			File file = new File(filename);
 			file.delete();
 		}
 	}
-	
+
 	@Test
 	public void execute() throws Exception {
 		Job job = jobRegistry.getJob(Constants.JOB_ID_EXPORT);
-		Map<String, JobParameter> params = new HashMap<String, JobParameter>();
+		Map<String, JobParameter> params = new HashMap<>();
 		params.put(Constants.JOB_PARAM_CONF_ID, new JobParameter(300L));
 		params.put(Constants.JOB_PARAM_OUT_FILE, new JobParameter(TEST_FILE_1));
 		params.put(Constants.JOB_PARAM_FORMAT, new JobParameter("line"));
 		JobParameters jobParams = new JobParameters(params);
 		jobLauncher.run(job, jobParams);
 	}
-	
+
 	@Test
 	public void testExportISO2709() throws Exception {
 		Job job = jobRegistry.getJob(Constants.JOB_ID_EXPORT);
-		Map<String, JobParameter> params = new HashMap<String, JobParameter>();
+		Map<String, JobParameter> params = new HashMap<>();
 		params.put(Constants.JOB_PARAM_CONF_ID, new JobParameter(300L));
 		params.put(Constants.JOB_PARAM_OUT_FILE, new JobParameter(TEST_FILE_2));
 		params.put(Constants.JOB_PARAM_FORMAT, new JobParameter("iso"));
 		JobParameters jobParams = new JobParameters(params);
 		jobLauncher.run(job, jobParams);
 	}
-	
+
 	@Test
 	public void testExportAleph() throws Exception {
 		Job job = jobRegistry.getJob(Constants.JOB_ID_EXPORT);
-		Map<String, JobParameter> params = new HashMap<String, JobParameter>();
+		Map<String, JobParameter> params = new HashMap<>();
 		params.put(Constants.JOB_PARAM_CONF_ID, new JobParameter(300L));
 		params.put(Constants.JOB_PARAM_OUT_FILE, new JobParameter(TEST_FILE_3));
 		params.put(Constants.JOB_PARAM_FORMAT, new JobParameter("aleph"));
 		JobParameters jobParams = new JobParameters(params);
 		jobLauncher.run(job, jobParams);
 	}
-	
+
+	@Test
+	public void testExportDublicCore() throws Exception {
+		Job job = jobRegistry.getJob(Constants.JOB_ID_EXPORT);
+		Map<String, JobParameter> params = new HashMap<>();
+		params.put(Constants.JOB_PARAM_CONF_ID, new JobParameter(304L));
+		params.put(Constants.JOB_PARAM_OUT_FILE, new JobParameter(TEST_FILE_5));
+		params.put(Constants.JOB_PARAM_FORMAT, new JobParameter("dcxml"));
+		JobParameters jobParams = new JobParameters(params);
+		jobLauncher.run(job, jobParams);
+	}
+
 	/**
 	 * test export marc record with alphabetic field identifier
 	 */
-	@Test 
+	@Test
 	public void testExportRecordsMarcAlpnaKey() throws Exception {
 		Job job = jobRegistry.getJob(Constants.JOB_ID_EXPORT);
-		Map<String, JobParameter> params = new HashMap<String, JobParameter>();
+		Map<String, JobParameter> params = new HashMap<>();
 		params.put(Constants.JOB_PARAM_CONF_ID, new JobParameter(301L));
 		params.put(Constants.JOB_PARAM_OUT_FILE, new JobParameter(TEST_FILE_4));
 		params.put(Constants.JOB_PARAM_FORMAT, new JobParameter("xml"));
 		JobParameters jobParams = new JobParameters(params);
 		jobLauncher.run(job, jobParams);
 	}
-	
+
 }
