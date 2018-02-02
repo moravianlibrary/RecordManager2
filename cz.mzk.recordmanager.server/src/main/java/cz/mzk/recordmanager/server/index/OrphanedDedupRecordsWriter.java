4a87952beff79164307bb657028eec30042d9bc0
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-02-02 11:09:18 +0100

    add logger to OrphanedDedupRecordsWriter

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/index/OrphanedDedupRecordsWriter.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/index/OrphanedDedupRecordsWriter.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.text.MessageFormat;
 import java.util.List;
 
+import cz.mzk.recordmanager.server.util.ProgressLogger;
 import org.apache.solr.client.solrj.SolrServerException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.ExitStatus;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.StepExecutionListener;
@@ -19,6 +22,10 @@ public class OrphanedDedupRecordsWriter implements ItemWriter<Long>, StepExecuti
 	@Autowired
 	private SolrServerFactory factory;
 
+	private static Logger logger = LoggerFactory.getLogger(OrphanedDedupRecordsWriter.class);
+
+	private ProgressLogger progressLogger;
+
 	private SolrServerFacade server;
 
 	private String solrUrl;
@@ -32,6 +39,7 @@ public class OrphanedDedupRecordsWriter implements ItemWriter<Long>, StepExecuti
 	@Override
 	public void write(List<? extends Long> items) throws Exception {
 		for (Long id : items) {
+			progressLogger.incrementAndLogProgress();
 			String query = MessageFormat.format("('{'!child of=merged_boolean:true'}'id:{0}) OR id:{0}", id.toString());
 			server.deleteByQuery(query, commitWithinMs);
 		}
@@ -40,11 +48,13 @@ public class OrphanedDedupRecordsWriter implements ItemWriter<Long>, StepExecuti
 	@Override
 	public void beforeStep(StepExecution stepExecution) {
 		server = factory.create(solrUrl);
+		progressLogger = new ProgressLogger(logger, 10000);
 	}
 
 	@Override
 	public ExitStatus afterStep(StepExecution stepExecution) {
 		try {
+			logger.info("Final commit");
 			server.commit();
 		} catch (SolrServerException | IOException ex) {
 			throw new RuntimeException("Final commit failed", ex);
