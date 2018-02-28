401cda95faa9aac506d367b20d14649a5c710d56
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-02-28 15:20:44 +0100

    update names of authors

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/AuthorityMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/AuthorityMarc.groovy
@@ -15,6 +15,6 @@ local_institution_facet_str_mv = getInstitutionFacet()
 
 authority_id_display = getId001()
 bibliographic_details_display_mv = getFields "678a"
-personal_name_display = getFirstField "100abd"
-alternative_name_display_mv = getFields "400abd"
+personal_name_display = getFirstAuthAuthor "100"
+alternative_name_display_mv = getAuthAuthors "400"
 source_display_mv = getFields "670a"
