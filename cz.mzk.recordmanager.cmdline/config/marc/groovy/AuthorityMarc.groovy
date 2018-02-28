2ccb8ec1b27bf458e62e20d107cd2dd23ecfe038
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-02-28 14:19:45 +0100

    remove unused solr fields

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/AuthorityMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/AuthorityMarc.groovy
@@ -2,22 +2,14 @@ recordtype = "authority"
 
 fullrecord = getFullrecord()
 
-source = getFields "670a"
-heading = getFirstAuthAuthor "100"
 heading_display = getFirstAuthAuthor "100"
-use_for = getAuthAuthors "400"
-see_also = getFields "500abcd"
-scope_note = getFields "678a"
 url = getUrls()
-
-id_authority = getId001()
 
 subject_facet_str_mv = getFields "100abcd"
 
 short_note_cs_display = translate("auth_short_note_cs.map", getId001(), null)
 short_note_en_display = translate("auth_short_note_en.map", getId001(), null)
 
-authorityId_str = getAuthorityId();
 format_display_mv = getFormat()
 local_institution_facet_str_mv = getInstitutionFacet()
 
