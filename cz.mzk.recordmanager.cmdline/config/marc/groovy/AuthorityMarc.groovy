334e774cb693683aa7b1e6a4f502792b9d52b19a
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-03-01 10:39:03 +0100

    sort fields

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/AuthorityMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/AuthorityMarc.groovy
@@ -1,18 +1,17 @@
 recordtype = "authority"
-
 fullrecord = getFullrecord()
-
 url = getUrls()
 
+// facets
+local_institution_facet_str_mv = getInstitutionFacet()
 subject_facet_str_mv = getFields "100abcd"
 
-format_display_mv = getFormat()
-local_institution_facet_str_mv = getInstitutionFacet()
-
+// display fields
+alternative_name_display_mv = getAuthAuthors "400"
 authority_id_display = getId001()
 bibliographic_details_display_mv = getFields "678a"
+format_display_mv = getFormat()
 personal_name_display = getFirstAuthAuthor "100"
-alternative_name_display_mv = getAuthAuthors "400"
-source_display_mv = getFields "670a"
-pseudonym_name_display_mv = getAuthorityPseudonymsNames()
 pseudonym_ids_display_mv = getAuthorityPseudonymsIds()
+pseudonym_name_display_mv = getAuthorityPseudonymsNames()
+source_display_mv = getFields "670a"
