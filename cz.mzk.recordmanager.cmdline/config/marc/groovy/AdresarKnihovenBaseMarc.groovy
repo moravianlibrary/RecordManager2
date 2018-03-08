c51f800eb3430ac3b489cd93eaeea3a62374f505
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-03-08 10:08:42 +0100

    adresarKnihoven: sort solr fields

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/AdresarKnihovenBaseMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/AdresarKnihovenBaseMarc.groovy
@@ -2,41 +2,41 @@ import cz.mzk.recordmanager.server.marc.SubfieldExtractionMethod;
 
 recordtype = "library"
 fullrecord = getFullrecord()
+library_relevance_str = getLibraryRelevance()
 
 // search
-town_search_txt = getFirstFieldForAdresar "MESa"
-region_search_txt = translate("adresar_region.map", getFirstFieldForAdresar("KRJa"), null)
-function_search_txt_mv = getFieldsForAdresar("FCEa", SubfieldExtractionMethod.SEPARATED, null)
-services_search_txt_mv = getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null)
-projects_search_txt_mv = getFieldsForAdresar("PRKa", SubfieldExtractionMethod.SEPARATED, null)
-type_search_txt_mv = getFieldsForAdresar "TYPb", SubfieldExtractionMethod.SEPARATED, null
-note_search_txt = getFirstFieldForAdresar "POIa"
-code_search_txt = getFirstFieldForAdresar "ZKRa"
-sigla_search_txt = getFirstFieldForAdresar "SGLa"
-responsibility_search_txt_mv = getFieldsForAdresar "JMNkp", SubfieldExtractionMethod.JOINED, " "
-note2_search_txt = getFirstFieldForAdresar "POUa"
-ils_search_txt = getFirstFieldForAdresar "KNSa"
 address_search_txt_mv = getFieldsForAdresar "ADRum", SubfieldExtractionMethod.JOINED, " "
 branch_search_txt_mv = getFieldsForAdresar "POBna", SubfieldExtractionMethod.JOINED, " "
-name_search_txt = getFirstFieldSeparatedForAdresar "NAZabc", " "
-name_alt_search_txt_mv = adresarGetNameAlt " "
+code_search_txt = getFirstFieldForAdresar "ZKRa"
 cpk_code_search_txt = adresarGetCpkCode()
+function_search_txt_mv = getFieldsForAdresar("FCEa", SubfieldExtractionMethod.SEPARATED, null)
+ils_search_txt = getFirstFieldForAdresar "KNSa"
+name_alt_search_txt_mv = adresarGetNameAlt " "
+name_search_txt = getFirstFieldSeparatedForAdresar "NAZabc", " "
+note2_search_txt = getFirstFieldForAdresar "POUa"
+note_search_txt = getFirstFieldForAdresar "POIa"
+projects_search_txt_mv = getFieldsForAdresar("PRKa", SubfieldExtractionMethod.SEPARATED, null)
+region_search_txt = translate("adresar_region.map", getFirstFieldForAdresar("KRJa"), null)
+responsibility_search_txt_mv = getFieldsForAdresar "JMNkp", SubfieldExtractionMethod.JOINED, " "
+services_search_txt_mv = getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null)
+sigla_search_txt = getFirstFieldForAdresar "SGLa"
+town_search_txt = getFirstFieldForAdresar "MESa"
+type_search_txt_mv = getFieldsForAdresar "TYPb", SubfieldExtractionMethod.SEPARATED, null
 
 // facets
-region_disctrict_facet_str_mv = adresarGetRegionDistrictFacet()
 function_facet_str_mv = getFieldsForAdresar("FCEa", SubfieldExtractionMethod.SEPARATED, null)
-services_facet_str_mv = getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null)
-projects_facet_str_mv = getFieldsForAdresar("PRKa", SubfieldExtractionMethod.SEPARATED, null)
-type_facet_str_mv = getFieldsForAdresar "TYPb", SubfieldExtractionMethod.SEPARATED, null
-library_relevance_str = getLibraryRelevance()
 portal_facet_str = getPortalFacet()
+projects_facet_str_mv = getFieldsForAdresar("PRKa", SubfieldExtractionMethod.SEPARATED, null)
+region_disctrict_facet_str_mv = adresarGetRegionDistrictFacet()
+services_facet_str_mv = getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null)
+type_facet_str_mv = getFieldsForAdresar "TYPb", SubfieldExtractionMethod.SEPARATED, null
 
 // morelikethis
 region_disctrict_town_str_mv = adresarGetRegionDistrictTown()
 
 // autocomplete
-town_str = town_autocomplete = getFirstFieldForAdresar "MESa"
 name_str = name_autocomplete = getFirstFieldSeparatedForAdresar "NAZabc", " "
+town_str = town_autocomplete = getFirstFieldForAdresar "MESa"
 
 // map
 address_map_display_mv = getFieldsForAdresar "ADRum", SubfieldExtractionMethod.JOINED, " "
--- cz.mzk.recordmanager.cmdline/config/marc/groovy/AdresarKnihovenLocal.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/AdresarKnihovenLocal.groovy
@@ -2,32 +2,32 @@ import cz.mzk.recordmanager.server.marc.SubfieldExtractionMethod;
 
 recordtype = "library"
 fullrecord = getFullrecord()
+library_relevance_str = getLibraryRelevance()
 
-sigla_display = getFirstFieldForAdresar "SGLa"
-name_display = getFirstFieldSeparatedForAdresar "NAZabc", " - "
-note_display = getFirstFieldForAdresar "POIa"
-type_display_mv = getFieldsForAdresar "TYPb", SubfieldExtractionMethod.SEPARATED, null
-phone_display_mv = getFieldsForAdresar "TELa", SubfieldExtractionMethod.SEPARATED, null
-note2_display = getFirstFieldForAdresar "POUa"
-branchurl_display_mv = getFieldsForAdresar "POBu", SubfieldExtractionMethod.SEPARATED, null
-branch_display_mv = getFieldsForAdresar "POBnamg", SubfieldExtractionMethod.JOINED, " | "
-url_display_mv = adresarGetUrlDisplay()
-function_display_mv = getFieldsForAdresar("FCEa", SubfieldExtractionMethod.SEPARATED, null)
-services_display_mv = getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null)
-projects_display_mv = getFieldsForAdresar("PRKa", SubfieldExtractionMethod.SEPARATED, null)
-responsibility_display_mv = adresarGetResponsibility()
+// display
 address_display_mv = adresarGetAddress()
-email_display_mv = adresarGetEmailOrMvs "EMLuz"
-mvs_display_mv = adresarGetEmailOrMvs "MVScu"
-reg_lib_display_mv = getFieldsForAdresar "PVKsn", SubfieldExtractionMethod.JOINED, " | "
-name_alt_display_mv = adresarGetNameAlt " - "
-hours_display = adresarGetHours()
+branch_display_mv = getFieldsForAdresar "POBnamg", SubfieldExtractionMethod.JOINED, " | "
+branchurl_display_mv = getFieldsForAdresar "POBu", SubfieldExtractionMethod.SEPARATED, null
 cpk_code_display = adresarGetCpkCode()
+email_display_mv = adresarGetEmailOrMvs "EMLuz"
+function_display_mv = getFieldsForAdresar("FCEa", SubfieldExtractionMethod.SEPARATED, null)
 gps_display = adresarGetGps()
+hours_display = adresarGetHours()
 ico_display = getFirstFieldSeparatedForAdresar "ICOab", " - "
 lastupdated_display = getFirstFieldForAdresar "AKTa"
-
-library_relevance_str = getLibraryRelevance()
+mvs_display_mv = adresarGetEmailOrMvs "MVScu"
+name_alt_display_mv = adresarGetNameAlt " - "
+name_display = getFirstFieldSeparatedForAdresar "NAZabc", " - "
+note2_display = getFirstFieldForAdresar "POUa"
+note_display = getFirstFieldForAdresar "POIa"
+phone_display_mv = getFieldsForAdresar "TELa", SubfieldExtractionMethod.SEPARATED, null
+projects_display_mv = getFieldsForAdresar("PRKa", SubfieldExtractionMethod.SEPARATED, null)
+reg_lib_display_mv = getFieldsForAdresar "PVKsn", SubfieldExtractionMethod.JOINED, " | "
+responsibility_display_mv = adresarGetResponsibility()
+services_display_mv = getFieldsForAdresar("SLUa", SubfieldExtractionMethod.SEPARATED, null)
+sigla_display = getFirstFieldForAdresar "SGLa"
+type_display_mv = getFieldsForAdresar "TYPb", SubfieldExtractionMethod.SEPARATED, null
+url_display_mv = adresarGetUrlDisplay()
 
 // remove later
 gps_str = adresarGetGps()
