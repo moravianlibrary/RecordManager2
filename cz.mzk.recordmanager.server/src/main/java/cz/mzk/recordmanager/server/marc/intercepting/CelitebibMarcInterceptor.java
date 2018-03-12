7d16a743c41cf4baa1b6c2aff657aecdef755cd5
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-03-12 11:09:23 +0100

    filter for CELITEBIB records

---
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/marc/intercepting/CelitebibMarcInterceptor.java
@@ -0,0 +1,52 @@
+package cz.mzk.recordmanager.server.marc.intercepting;
+
+import cz.mzk.recordmanager.server.export.IOFormat;
+import cz.mzk.recordmanager.server.marc.MarcRecordImpl;
+import cz.mzk.recordmanager.server.marc.marc4j.MarcFactoryImpl;
+import cz.mzk.recordmanager.server.marc.marc4j.RecordImpl;
+import org.marc4j.marc.*;
+
+import java.nio.charset.StandardCharsets;
+import java.util.regex.Pattern;
+
+public class CelitebibMarcInterceptor extends DefaultMarcInterceptor {
+
+	private static final Pattern FIELD_START = Pattern.compile("^[167]..$");
+	private static final MarcFactory factory = new MarcFactoryImpl();
+
+	public CelitebibMarcInterceptor(Record record) {
+		super(record);
+	}
+
+	@Override
+	public byte[] intercept() {
+		if (super.getRecord() == null) {
+			return new byte[0];
+		}
+		Record newRecord = new RecordImpl();
+		newRecord.setLeader(getRecord().getLeader());
+		for (ControlField cf : super.getRecord().getControlFields()) {
+			newRecord.addVariableField(cf);
+		}
+
+		for (DataField df : super.getRecord().getDataFields()) {
+			// remove subfield 'x' from fields 1xx, 6xx, 7xx
+			if (FIELD_START.matcher(df.getTag()).matches()) {
+				DataField newDf = factory.newDataField(df.getTag(), df.getIndicator1(), df.getIndicator2());
+				for (Subfield subfield : df.getSubfields()) {
+					if (subfield.getCode() == 'x') continue;
+					newDf.addSubfield(subfield);
+				}
+				newRecord.addVariableField(newDf);
+				continue;
+			}
+			// remove field 964
+			if (df.getTag().equals("964")) continue;
+			// default
+			newRecord.addVariableField(df);
+		}
+
+		return new MarcRecordImpl(newRecord).export(IOFormat.XML_MARC).getBytes(StandardCharsets.UTF_8);
+	}
+
+}
