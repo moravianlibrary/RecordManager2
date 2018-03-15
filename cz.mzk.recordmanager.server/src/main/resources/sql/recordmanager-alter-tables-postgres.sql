f4a898d78d08fdc5998dcb8b6dbafa7c86b628fb
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-03-15 13:45:07 +0100

    update MUZIBIB base_weight

--- cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-alter-tables-postgres.sql
+++ cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-alter-tables-postgres.sql
@@ -1018,3 +1018,6 @@ INSERT INTO harvested_record_format(id, name) VALUES (66, 'BLIND_BRAILLE');
 
 -- 12. 03. 2018 tomascejpek
 UPDATE import_conf SET interception_enabled=true WHERE id=360;
+
+-- 15. 03. 2018 tomascejpek
+UPDATE import_conf SET base_weight=11 WHERE id=349;
--- cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-liquibase.sql
+++ cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-liquibase.sql
@@ -1283,3 +1283,6 @@ INSERT INTO harvested_record_format(id, name) VALUES (66, 'BLIND_BRAILLE');
 
 --changeset tomascejpek:92 context:cpk
 UPDATE import_conf SET interception_enabled=true WHERE id=360;
+
+--changeset tomascejpek:93 context:cpk
+UPDATE import_conf SET base_weight=11 WHERE id=349;
