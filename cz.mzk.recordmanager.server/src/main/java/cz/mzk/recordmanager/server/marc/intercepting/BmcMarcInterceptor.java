9fc5379769202935ae1fbd99172d5d99ba899a37
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-03-27 08:48:03 +0200

    udpate BMC interceptor

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/marc/intercepting/BmcMarcInterceptor.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/marc/intercepting/BmcMarcInterceptor.java
@@ -1,20 +1,22 @@
 package cz.mzk.recordmanager.server.marc.intercepting;
 
+import cz.mzk.recordmanager.server.export.IOFormat;
+import cz.mzk.recordmanager.server.marc.MarcRecord;
+import cz.mzk.recordmanager.server.marc.MarcRecordImpl;
+import cz.mzk.recordmanager.server.marc.marc4j.RecordImpl;
+import org.marc4j.marc.ControlField;
+import org.marc4j.marc.DataField;
+import org.marc4j.marc.Record;
+
 import java.nio.charset.StandardCharsets;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeSet;
 
-import org.marc4j.marc.ControlField;
-import org.marc4j.marc.DataField;
-import org.marc4j.marc.Record;
-
-import cz.mzk.recordmanager.server.export.IOFormat;
-import cz.mzk.recordmanager.server.marc.MarcRecord;
-import cz.mzk.recordmanager.server.marc.MarcRecordImpl;
-import cz.mzk.recordmanager.server.marc.marc4j.RecordImpl;
-
 public class BmcMarcInterceptor extends DefaultMarcInterceptor {
+
+	private static final String LINK_STR = "https://www.medvik.cz/bmc/link.do?id=%s";
+	private static final String LINK_SF_Y = "záznam v BMČ";
 
 	public BmcMarcInterceptor(Record record) {
 		super(record);
@@ -35,11 +37,18 @@ public class BmcMarcInterceptor extends DefaultMarcInterceptor {
 		}
 
 		Map<String, List<DataField>> dfMap = marc.getAllFields();
-		for (String tag : new TreeSet<String>(dfMap.keySet())) { // sorted tags
+		for (String tag : new TreeSet<>(dfMap.keySet())) { // sorted tags
 			for (DataField df : dfMap.get(tag)) {
 				// remove field 990, 991
 				if (!df.getTag().equals("990") && !df.getTag().equals("991")) newRecord.addVariableField(df);
 			}
+		}
+		// add 856 as link + 001
+		if (newRecord.getControlNumber() != null) {
+			DataField linkDf = MARC_FACTORY.newDataField("856", '4', '0',
+					"u", String.format(LINK_STR, newRecord.getControlNumber()),
+					"y", LINK_SF_Y);
+			newRecord.addVariableField(linkDf);
 		}
 		return new MarcRecordImpl(newRecord).export(IOFormat.XML_MARC).getBytes(StandardCharsets.UTF_8);
 	}
