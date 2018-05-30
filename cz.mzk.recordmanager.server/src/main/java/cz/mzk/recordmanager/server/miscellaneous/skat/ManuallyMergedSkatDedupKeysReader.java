d232f656fc61890b9edffd611d4eeecbe6fda2e6
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-05-30 12:46:48 +0200

    skat keys refactoring

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/miscellaneous/skat/ManuallyMergedSkatDedupKeysReader.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/miscellaneous/skat/ManuallyMergedSkatDedupKeysReader.java
@@ -25,8 +25,6 @@ import java.util.regex.Pattern;
 
 public class ManuallyMergedSkatDedupKeysReader implements ItemReader<Set<SkatKey>> {
 
-	private ApacheHttpClient httpClient;
-
 	@Autowired
 	private SessionFactory sessionFactory;
 
@@ -36,32 +34,31 @@ public class ManuallyMergedSkatDedupKeysReader implements ItemReader<Set<SkatKey
 	@Autowired
 	private HarvestedRecordDAO hrDao;
 
-	private static Logger logger = LoggerFactory
-			.getLogger(ManuallyMergedSkatDedupKeysReader.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(ManuallyMergedSkatDedupKeysReader.class);
 
 	private static final String SKC_ID_PREFIX = "SKC01-";
-
 	private static final String ALEPH_URL_QUERY = "https://aleph.nkp.cz/F/";
 	private static final String ALEPH_URL_REGEX = "http[s]://aleph.nkp.cz/";
 
-	private static final Pattern PATTERN = Pattern
+	private static final Pattern CONTEXT_ID = Pattern
 			.compile(ALEPH_URL_REGEX + "F/([A-Z0-9-]*)\\?func=short-mail-0");
-	private static final Pattern PATTERN2 = Pattern
-			.compile("(" + ALEPH_URL_REGEX + "exlibris/aleph/a22_1/tmp/[^.]*\\.sav)");
+	private static final Pattern FILE_URL = Pattern
+			.compile('(' + ALEPH_URL_REGEX + "exlibris/aleph/a22_1/tmp/[^.]*\\.sav)");
 	private static final Pattern SYSNO = Pattern.compile("System.cislo\\s*([0-9]+)");
+
+	private static final Map<String, String> ALEPH_MAIL_PARAMS = createAlephMailParams();
+	private static final Map<String, String> HEADERS = Collections.singletonMap("User-Agent",
+			"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0");
+
+	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
 
 	private Date toDate = null;
 	private Date counterDate = null;
-	private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd");
 	private Set<String> downloadedKeys = new HashSet<>();
-	private Map<String, String> headers;
 
 	public ManuallyMergedSkatDedupKeysReader(Date fromDate, Date toDate) {
-		counterDate = fromDate;
+		this.counterDate = fromDate;
 		this.toDate = toDate;
-		this.headers = Collections
-				.singletonMap("User-Agent",
-						"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:48.0) Gecko/20100101 Firefox/48.0");
 	}
 
 	@Override
@@ -72,17 +69,17 @@ public class ManuallyMergedSkatDedupKeysReader implements ItemReader<Set<SkatKey
 			toDate = new Date();
 		}
 		toDate = DateUtils.truncate(toDate, Calendar.DAY_OF_MONTH);
-		while (!counterDate.after(toDate)) {
+		if (!counterDate.after(toDate)) {
 			downloadedKeys.clear();
 			String get = IOUtils.toString(harvest(prepareAlephBaseUrl(DATE_FORMAT.format(counterDate))));
 
-			if (!(matcher = PATTERN.matcher(get)).find()) {
-				logger.info("Session not found!!!");
+			if (!(matcher = CONTEXT_ID.matcher(get)).find()) {
+				LOGGER.info("Session not found!!!");
 			} else {
 				sleep(20000, 30000); // wait 20 - 30 seconds
 				get = IOUtils.toString(harvest(prepareAlephMailUrl(matcher.group(1))));
-				if (!(matcher = PATTERN2.matcher(get)).find()) {
-					logger.info("File with results not found!!!");
+				if (!(matcher = FILE_URL.matcher(get)).find()) {
+					LOGGER.info("File with results not found!!!");
 				} else {
 					sleep(20000, 30000); // wait 20 - 30 seconds
 					matcher = SYSNO.matcher(IOUtils.toString(harvest(matcher.group(1))));
@@ -96,7 +93,7 @@ public class ManuallyMergedSkatDedupKeysReader implements ItemReader<Set<SkatKey
 			counterDate = DateUtils.addDays(counterDate, 1); // next day
 			if (counterDate.before(toDate)) sleep(120000, 180000); // wait 2-3 minutes
 			Set<SkatKey> results = new HashSet<>();
-			//get skat keys 
+			//get skat keys
 			downloadedKeys.forEach(key -> {
 				HarvestedRecord hr = hrDao.findByIdAndHarvestConfiguration(key, Constants.IMPORT_CONF_ID_CASLIN);
 				if (hr != null) {
@@ -111,49 +108,47 @@ public class ManuallyMergedSkatDedupKeysReader implements ItemReader<Set<SkatKey
 		return null;
 	}
 
-	protected void sleep(int min, int max) {
+	private static void sleep(int min, int max) {
 		try {
 			int time = min + (int) (Math.random() * ((max - min) + 1));
-			logger.info("Waiting: " + time / 1000 + " seconds");
+			LOGGER.info("Waiting: " + time / 1000 + " seconds");
 			Thread.sleep(time);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 	}
 
-	protected InputStream harvest(String url) throws IOException {
-		logger.info("Harvesting from: " + url);
-		httpClient = new ApacheHttpClient();
-		return httpClient.executeGet(url, headers);
+	private static InputStream harvest(String url) throws IOException {
+		LOGGER.info("Harvesting from: " + url);
+		ApacheHttpClient httpClient = new ApacheHttpClient();
+		return httpClient.executeGet(url, HEADERS);
 	}
 
-	protected void pushToDatabase() {
+	private void pushToDatabase() {
 		for (String currentKey : downloadedKeys) {
 			push(currentKey);
 		}
 	}
 
-	protected void push(String recordId) {
-		String query = "UPDATE harvested_record SET next_dedup_flag=true WHERE import_conf_id = ? AND record_id = ?";
+	private void push(String recordId) {
+		String query = "UPDATE harvested_record SET next_dedup_flag=TRUE WHERE import_conf_id = ? AND record_id = ?";
 		Session session = sessionFactory.getCurrentSession();
 		session.createSQLQuery(query)
 				.setLong(0, Constants.IMPORT_CONF_ID_CASLIN)
 				.setString(1, recordId).executeUpdate();
 	}
 
-	protected String prepareAlephBaseUrl(String date) {
+	private static String prepareAlephBaseUrl(String date) {
 		Map<String, String> params = new HashMap<>();
 		params.put("func", "find-c");
-		params.put("ccl_term", "ia=sl" + date + "*");
+		params.put("ccl_term", "ia=sl" + date + '*');
 		params.put("adjacent", "N");
 		params.put("local_base", "SKC");
 
 		return UrlUtils.buildUrl(ALEPH_URL_QUERY, params);
 	}
 
-	protected String prepareAlephMailUrl(String context) {
-		String url = ALEPH_URL_QUERY + context;
-
+	private static Map<String, String> createAlephMailParams() {
 		Map<String, String> params = new HashMap<>();
 		params.put("func", "short-mail");
 		params.put("records", "ALL");
@@ -164,8 +159,11 @@ public class ManuallyMergedSkatDedupKeysReader implements ItemReader<Set<SkatKey
 		params.put("SUBJECT", "");
 		params.put("x", "32");
 		params.put("y", "11");
+		return params;
+	}
 
-		return UrlUtils.buildUrl(url, params);
+	private static String prepareAlephMailUrl(String context) {
+		return UrlUtils.buildUrl(ALEPH_URL_QUERY + context, ALEPH_MAIL_PARAMS);
 	}
 
 }
