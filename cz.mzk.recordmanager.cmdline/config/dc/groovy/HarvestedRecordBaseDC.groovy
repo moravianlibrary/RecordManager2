2d4f8293e67b2b33153de3b409f4985004beffd9
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-07-30 13:10:51 +0200

    add display fields

--- cz.mzk.recordmanager.cmdline/config/dc/groovy/HarvestedRecordBaseDC.groovy
+++ cz.mzk.recordmanager.cmdline/config/dc/groovy/HarvestedRecordBaseDC.groovy
@@ -1,26 +1,26 @@
 recordtype = "dublincore"
 fullrecord = getFullRecord()
+title = getFirstTitle()
+physical = getPhysicals();
+url = getUrls();
+barcodes = getBarcodes()
+_hidden_index_when_merged_boolean = getIndexWhenMerged()
 
+// facet
+local_institution_facet_str_mv = getInstitutionFacet()
 local_statuses_facet_str_mv = getStatuses()
 
-title = getFirstTitle()
-title_display = getFirstTitle()
-
-author_display = getAuthorDisplay()
-author2_display_mv = getAuthor2Display()
-
-publishDate_display = getFirstDate()
-
-isbn_display_mv = getISBNs()
-oclc_display_mv = getOclcs()
-
-physical = getPhysicals();
-
-url = getUrls();
-
-barcodes = getBarcodes()
-format_display_mv = getFormat()
-local_institution_facet_str_mv = getInstitutionFacet()
-_hidden_index_when_merged_boolean = getIndexWhenMerged()
+// search
 author_search_txt_mv = getAllCreatorsForSearching()
 title_search_txt_mv = getFirstTitle()
+
+// display
+author2_display_mv = getAuthor2Display()
+author_display = getAuthorDisplay()
+format_display_mv = getFormat()
+isbn_display_mv = getISBNs()
+language_display_mv = getLanguages()
+oclc_display_mv = getOclcs()
+publishDate_display = getFirstDate()
+publisher_display_mv = getPublishers()
+title_display = getFirstTitle()
