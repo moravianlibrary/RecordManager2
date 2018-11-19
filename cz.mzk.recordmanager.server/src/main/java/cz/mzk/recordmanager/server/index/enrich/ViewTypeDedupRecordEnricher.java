3ae1fd8f5d83d1dd42ef7b4136806bfed123561c
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-11-19 12:53:33 +0100

    filter IREL view

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/index/enrich/ViewTypeDedupRecordEnricher.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/index/enrich/ViewTypeDedupRecordEnricher.java
@@ -8,7 +8,9 @@ import cz.mzk.recordmanager.server.util.SolrUtils;
 import org.apache.solr.common.SolrInputDocument;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -17,8 +19,16 @@ public class ViewTypeDedupRecordEnricher implements DedupRecordEnricher {
 	private static final FieldMerger fieldMerger = new FieldMerger(
 			SolrFieldConstants.VIEW_TYPE_TXT_MV);
 
-	private static final String MUSICAL_SCORE = SolrUtils.createHierarchicFacetValues(
-			HarvestedRecordFormatEnum.MUSICAL_SCORES.name()).get(0);
+	private static final List<String> MUSICAL_SCORE = SolrUtils.createHierarchicFacetValues(
+			HarvestedRecordFormatEnum.MUSICAL_SCORES.name());
+	private static final List<String> BLIND_BRAILLE = SolrUtils.createHierarchicFacetValues(
+			HarvestedRecordFormatEnum.BLIND_BRAILLE.name());
+	private static final List<String> VISUAL_DOCUMENTS = SolrUtils.createHierarchicFacetValues(
+			HarvestedRecordFormatEnum.VISUAL_DOCUMENTS.name());
+	private static final List<String> REMOVE_FORMATS = Arrays.asList(
+			MUSICAL_SCORE.get(MUSICAL_SCORE.size() - 1),
+			BLIND_BRAILLE.get(BLIND_BRAILLE.size() - 1),
+			VISUAL_DOCUMENTS.get(VISUAL_DOCUMENTS.size() - 1));
 
 	@Override
 	public void enrich(DedupRecord record, SolrInputDocument mergedDocument,
@@ -26,7 +36,7 @@ public class ViewTypeDedupRecordEnricher implements DedupRecordEnricher {
 		if (localRecords.stream().anyMatch(rec -> rec.containsKey(SolrFieldConstants.VIEW_TYPE_TXT_MV)
 				&& rec.getFieldValues(SolrFieldConstants.VIEW_TYPE_TXT_MV).contains(ViewType.IREL.getValue()))
 				&& localRecords.stream().anyMatch(rec -> rec.containsKey(SolrFieldConstants.RECORD_FORMAT_DISPLAY)
-				&& rec.getFieldValues(SolrFieldConstants.RECORD_FORMAT_DISPLAY).contains(MUSICAL_SCORE))) {
+				&& !Collections.disjoint(rec.getFieldValues(SolrFieldConstants.RECORD_FORMAT_DISPLAY), REMOVE_FORMATS))) {
 			for (SolrInputDocument local : localRecords) {
 				if (local.containsKey(SolrFieldConstants.VIEW_TYPE_TXT_MV)) {
 					Collection<Object> views = local.getFieldValues(SolrFieldConstants.VIEW_TYPE_TXT_MV);
