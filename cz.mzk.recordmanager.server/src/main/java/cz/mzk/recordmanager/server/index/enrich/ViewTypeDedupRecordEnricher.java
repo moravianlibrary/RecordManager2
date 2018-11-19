3b295d4d4ed41fb273eb1ac5c31eb36e8fbe2db0
Tomáš Cejpek <tomas.cejpek@email.cz>
2018-11-19 12:02:45 +0100

    filter IREL view

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/index/enrich/ViewTypeDedupRecordEnricher.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/index/enrich/ViewTypeDedupRecordEnricher.java
@@ -1,10 +1,14 @@
 package cz.mzk.recordmanager.server.index.enrich;
 
 import cz.mzk.recordmanager.server.index.SolrFieldConstants;
+import cz.mzk.recordmanager.server.metadata.view.ViewType;
 import cz.mzk.recordmanager.server.model.DedupRecord;
+import cz.mzk.recordmanager.server.model.HarvestedRecordFormat.HarvestedRecordFormatEnum;
+import cz.mzk.recordmanager.server.util.SolrUtils;
 import org.apache.solr.common.SolrInputDocument;
 import org.springframework.stereotype.Component;
 
+import java.util.Collection;
 import java.util.List;
 
 @Component
@@ -13,9 +17,25 @@ public class ViewTypeDedupRecordEnricher implements DedupRecordEnricher {
 	private static final FieldMerger fieldMerger = new FieldMerger(
 			SolrFieldConstants.VIEW_TYPE_TXT_MV);
 
+	private static final String MUSICAL_SCORE = SolrUtils.createHierarchicFacetValues(
+			HarvestedRecordFormatEnum.MUSICAL_SCORES.name()).get(0);
+
 	@Override
 	public void enrich(DedupRecord record, SolrInputDocument mergedDocument,
 					List<SolrInputDocument> localRecords) {
+		if (localRecords.stream().anyMatch(rec -> rec.containsKey(SolrFieldConstants.VIEW_TYPE_TXT_MV)
+				&& rec.getFieldValues(SolrFieldConstants.VIEW_TYPE_TXT_MV).contains(ViewType.IREL.getValue()))
+				&& localRecords.stream().anyMatch(rec -> rec.containsKey(SolrFieldConstants.RECORD_FORMAT_DISPLAY)
+				&& rec.getFieldValues(SolrFieldConstants.RECORD_FORMAT_DISPLAY).contains(MUSICAL_SCORE))) {
+			for (SolrInputDocument local : localRecords) {
+				if (local.containsKey(SolrFieldConstants.VIEW_TYPE_TXT_MV)) {
+					Collection<Object> views = local.getFieldValues(SolrFieldConstants.VIEW_TYPE_TXT_MV);
+					views.remove(ViewType.IREL.getValue());
+					local.setField(SolrFieldConstants.VIEW_TYPE_TXT_MV, views);
+				}
+			}
+		}
+
 		fieldMerger.merge(localRecords, mergedDocument);
 	}
 
