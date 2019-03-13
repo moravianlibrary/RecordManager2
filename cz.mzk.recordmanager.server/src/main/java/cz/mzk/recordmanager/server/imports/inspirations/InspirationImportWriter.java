00ce6dfc5c5a97a08e51f86a2de81651e948a29e
Tomáš Cejpek <tomas.cejpek@email.cz>
2019-03-13 11:04:56 +0100

    more effective import of inspirations

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/imports/inspirations/InspirationImportWriter.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/imports/inspirations/InspirationImportWriter.java
@@ -1,12 +1,11 @@
 package cz.mzk.recordmanager.server.imports.inspirations;
 
-import java.util.Date;
-import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
+import cz.mzk.recordmanager.server.model.HarvestedRecord;
+import cz.mzk.recordmanager.server.model.ImportConfiguration;
+import cz.mzk.recordmanager.server.model.Inspiration;
+import cz.mzk.recordmanager.server.oai.dao.HarvestedRecordDAO;
+import cz.mzk.recordmanager.server.oai.dao.ImportConfigurationDAO;
+import cz.mzk.recordmanager.server.oai.dao.InspirationDAO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
@@ -14,12 +13,14 @@ import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import cz.mzk.recordmanager.server.model.HarvestedRecord;
-import cz.mzk.recordmanager.server.model.ImportConfiguration;
-import cz.mzk.recordmanager.server.model.Inspiration;
-import cz.mzk.recordmanager.server.oai.dao.HarvestedRecordDAO;
-import cz.mzk.recordmanager.server.oai.dao.ImportConfigurationDAO;
-import cz.mzk.recordmanager.server.oai.dao.InspirationDAO;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Component
 @StepScope
@@ -57,10 +58,17 @@ public class InspirationImportWriter implements ItemWriter<Map<String, List<Stri
 				logger.info(String.format(TEXT_INFO, inspiration_name, entry.getValue().size()));
 				// actual list of records with inspiration in db
 				List<HarvestedRecord> hrWithInspiration = inspirationDao.fingHrByInspiraion(inspiration_name);
+				List<String> idsOfHrWithInspiration = hrWithInspiration.stream().map(
+						hr -> hr.getHarvestedFrom().getIdPrefix() + "." + hr.getUniqueId().getRecordId())
+						.collect(Collectors.toList());
+				List<String> toDelete = new ArrayList<>(idsOfHrWithInspiration);
+				toDelete.removeAll(entry.getValue());
+				List<String> toAdd = new ArrayList<>(entry.getValue());
+				toAdd.removeAll(idsOfHrWithInspiration);
 				int added_ins = 0; // added inspiration records
-				int exists_ins = 0; // inspirations in db
+				int exists_ins = entry.getValue().size() - toAdd.size(); // inspirations in db
 				int not_exists_rec = 0; // hr not in db
-				for (String id : entry.getValue()) {
+				for (String id : toAdd) {
 					Matcher matcher = PATTERN_ID.matcher(id);
 					if (matcher.matches()) {
 						String id_prefix = matcher.group(1);
@@ -74,25 +82,21 @@ public class InspirationImportWriter implements ItemWriter<Map<String, List<Stri
 								if (++counter == confs.size()) ++not_exists_rec;
 								continue;
 							}
-
-							if (hrWithInspiration.contains(hr)) {
-								// inspiration is already in db
-								hrWithInspiration.remove(hr);
-								++exists_ins;
-							} else { // add inspiration to hr
-								List<Inspiration> result = hr.getInspiration();
-								Inspiration newInspiration = new Inspiration(entry.getKey());
-								newInspiration.setHarvestedRecordId(hr.getId());
-								result.add(newInspiration);
-								hr.setInspiration(result);
-								hr.setUpdated(new Date());
-								hrDao.persist(hr);
-								++added_ins;
-							}
+							// add inspiration to hr
+							List<Inspiration> result = hr.getInspiration();
+							Inspiration newInspiration = new Inspiration(entry.getKey());
+							newInspiration.setHarvestedRecordId(hr.getId());
+							result.add(newInspiration);
+							hr.setInspiration(result);
+							hr.setUpdated(new Date());
+							hrDao.persist(hr);
+							++added_ins;
 						}
 					}
 				}
 				// rest of records - delete inspiration
+				hrWithInspiration.removeIf(hr -> !toDelete.contains(
+						hr.getHarvestedFrom().getIdPrefix() + "." + hr.getUniqueId().getRecordId()));
 				for (HarvestedRecord hr : hrWithInspiration) {
 					Inspiration delete = inspirationDao.findByHrIdAndName(hr.getId(), inspiration_name);
 					List<Inspiration> inspirations = hr.getInspiration();
