0057e325a084377b48a1e39d5aaa158bea35ea9b
Tomáš Cejpek <tomas.cejpek@email.cz>
2019-10-16 11:14:43 +0200

    fix bibliolinker flags

--- cz.mzk.recordmanager.server/src/main/resources/job/biblioLinkerJob/initBiblioLinkerSimilar.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/biblioLinkerJob/initBiblioLinkerSimilar.sql
@@ -3,12 +3,12 @@ CREATE SEQUENCE tmp_bl_id_seq;
 
 DROP TABLE IF EXISTS tmp_bl_flag_ids;
 CREATE TABLE tmp_bl_flag_ids AS
-SELECT id FROM harvested_record WHERE next_biblio_linker_flag IS TRUE;
+SELECT id FROM harvested_record WHERE next_biblio_linker_similar_flag IS TRUE;
 
 DELETE FROM biblio_linker_similar bls WHERE
   EXISTS (SELECT 1 FROM tmp_bl_flag_ids tmp WHERE tmp.id=bls.harvested_record_id);
 
-UPDATE harvested_record hr SET next_biblio_linker_flag=TRUE WHERE id IN (
+UPDATE harvested_record hr SET next_biblio_linker_similar_flag=TRUE WHERE id IN (
   SELECT harvested_record_id FROM biblio_linker_similar bls WHERE
   EXISTS (SELECT 1 FROM tmp_bl_flag_ids tmp WHERE tmp.id=bls.harvested_record_similar_id));
 
