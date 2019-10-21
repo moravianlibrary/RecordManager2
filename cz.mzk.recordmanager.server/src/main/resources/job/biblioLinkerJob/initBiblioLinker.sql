76efce53ac28fb884e92b5be4321ff6bfd745464
Tomáš Cejpek <tomas.cejpek@email.cz>
2019-10-21 14:16:51 +0200

    add missing tables for BL

--- cz.mzk.recordmanager.server/src/main/resources/job/biblioLinkerJob/initBiblioLinker.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/biblioLinkerJob/initBiblioLinker.sql
@@ -6,5 +6,19 @@ SET updated=localtimestamp
 WHERE id IN (SELECT DISTINCT dedup_record_id FROM harvested_record WHERE next_biblio_linker_flag=TRUE AND biblio_linker_id IS NOT NULL);
 
 UPDATE harvested_record
-SET biblio_linker_id=NULL,next_biblio_linker_flag=TRUE
+SET biblio_linker_id=NULL,next_biblio_linker_flag=TRUE,next_biblio_linker_similar_flag=TRUE
 WHERE biblio_linker_id IN (SELECT DISTINCT biblio_linker_id FROM harvested_record WHERE next_biblio_linker_flag=TRUE AND biblio_linker_id IS NOT NULL);
+
+DROP TABLE IF EXISTS tmp_video_ids;
+CREATE TABLE tmp_video_ids AS
+  SELECT hrfl.harvested_record_id AS id FROM harvested_record_format_link hrfl
+    INNER JOIN harvested_record_format hrf ON hrf.id = hrfl.harvested_record_format_id
+  WHERE hrf.name ~* '^VIDEO';
+CREATE INDEX tmp_video_ids_idx ON tmp_video_ids(id);
+
+DROP TABLE IF EXISTS tmp_audio_ids;
+CREATE TABLE tmp_audio_ids AS
+  SELECT hrfl.harvested_record_id AS id FROM harvested_record_format_link hrfl
+    INNER JOIN harvested_record_format hrf ON hrf.id = hrfl.harvested_record_format_id
+  WHERE hrf.name ~* '^AUDIO';
+CREATE INDEX tmp_audio_ids_idx ON tmp_audio_ids(id);
