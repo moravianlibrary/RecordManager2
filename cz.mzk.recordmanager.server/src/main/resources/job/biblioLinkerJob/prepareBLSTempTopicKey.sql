546779f34fdf97d678a562e6aad6f14a11550a75
Tomáš Cejpek <tomas.cejpek@email.cz>
2019-10-21 14:47:39 +0200

    topic_key is table, not column

--- cz.mzk.recordmanager.server/src/main/resources/job/biblioLinkerJob/prepareBLSTempTopicKey.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/biblioLinkerJob/prepareBLSTempTopicKey.sql
@@ -4,14 +4,14 @@ CREATE TABLE tmp_bls_topic_key AS
 SELECT nextval('tmp_bl_id_seq') AS row_id,
   array_to_string(array_agg(DISTINCT hr.biblio_linker_id), ',') biblio_linker_id,
   array_to_string(array_agg(hr.id), ',') local_record_id,
-  hr.bl_topic_key
+  tk.topic_key
 FROM harvested_record hr
   INNER JOIN harvested_record_format_link hrfl ON hr.id = hrfl.harvested_record_id
+  INNER JOIN bl_topic_key tk ON hr.id=tk.harvested_record_id
   INNER JOIN language l ON l.harvested_record_id = hr.id
-WHERE hr.bl_topic_key IS NOT NULL
-      AND l.lang='cze' AND hrfl.harvested_record_format_id IN (1,2,3,4,5,12,13,14,15,16,17,18,19,20,21,22,23,27,28,61,62,63)
+WHERE l.lang='cze' AND hrfl.harvested_record_format_id IN (1,2,3,4,5,12,13,14,15,16,17,18,19,20,21,22,23,27,28,61,62,63)
       AND biblio_linker_similar IS TRUE
-GROUP BY hr.bl_topic_key
+GROUP BY tk.topic_key
 HAVING COUNT(DISTINCT biblio_linker_id)>1
   AND bool_or(next_biblio_linker_similar_flag) IS TRUE
   AND COUNT(hr.id)<10000
