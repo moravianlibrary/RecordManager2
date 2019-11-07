7fc7f0fb0bd3c4250754331c7831ef1cd60cb1c0
Tomáš Cejpek <tomas.cejpek@email.cz>
2019-11-07 13:19:43 +0100

    update name of title key in disadvantaged dedup steps

--- cz.mzk.recordmanager.server/src/main/resources/job/dedupRecordsJob/prepareTempDisadvantagedPagesTable.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/dedupRecordsJob/prepareTempDisadvantagedPagesTable.sql
@@ -3,15 +3,15 @@ DROP TABLE IF EXISTS tmp_disadvantaged_pages;
 CREATE TABLE tmp_disadvantaged_pages AS
 SELECT
   nextval('tmp_table_id_seq') AS row_id,
-  t.short_title,
+  t.anp_title,
   hr.author_string,
   hr.pages,
   hr.publication_year,
   array_to_string(array_agg(hr.id), ',')  id_array
 FROM harvested_record hr
-  INNER JOIN short_title t ON hr.id = t.harvested_record_id
+  INNER JOIN anp_title t ON hr.id = t.harvested_record_id
 WHERE hr.disadvantaged IS TRUE AND hr.author_string IS NOT NULL AND hr.pages IS NOT NULL AND hr.publication_year IS NOT NULL
-GROUP BY t.short_title,hr.author_string,hr.pages,hr.publication_year
+GROUP BY t.anp_title,hr.author_string,hr.pages,hr.publication_year
 HAVING COUNT(DISTINCT hr.id) > 1 
   AND COUNT(DISTINCT dedup_record_id) + SUM(CASE WHEN dedup_record_id IS NULL THEN 1 ELSE 0 END) != 1
   AND bool_or(next_dedup_flag) IS TRUE;
