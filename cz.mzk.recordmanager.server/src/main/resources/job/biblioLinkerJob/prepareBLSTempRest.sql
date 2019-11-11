8803dd49a982812a39960b06689bfb609dd94849
Tomáš Cejpek <tomas.cejpek@email.cz>
2019-11-11 06:43:39 +0100

    fix potential null pointer

--- cz.mzk.recordmanager.server/src/main/resources/job/biblioLinkerJob/prepareBLSTempRest.sql
+++ cz.mzk.recordmanager.server/src/main/resources/job/biblioLinkerJob/prepareBLSTempRest.sql
@@ -5,9 +5,9 @@ SELECT nextval('tmp_bl_id_seq') AS row_id,
   array_to_string(array_agg(DISTINCT hr.biblio_linker_id), ',') biblio_linker_id,
   array_to_string(array_agg(hr.id), ',') local_record_id
 FROM harvested_record hr
-WHERE hr.deleted IS NULL
+WHERE hr.biblio_linker_id IS NULL AND hr.deleted IS NULL
 GROUP BY hr.biblio_linker_id
 HAVING COUNT(DISTINCT hr.id)>1
-  AND bool_or(next_biblio_linker_similar_flag) IS TRUE;
+  AND BOOL_OR(next_biblio_linker_similar_flag) IS TRUE;
 
 CREATE INDEX tmp_bls_rest_idx ON tmp_bls_rest(row_id);
