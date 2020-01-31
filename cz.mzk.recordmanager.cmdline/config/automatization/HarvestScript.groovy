a51a3aa85a3ebba18a62bc2b2ce6791fa48c10ac
Tomáš Cejpek <tomas.cejpek@email.cz>
2020-01-31 13:13:59 +0100

    add script for harvesting

---
+++ cz.mzk.recordmanager.cmdline/config/automatization/HarvestScript.groovy
@@ -0,0 +1,45 @@
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import cz.mzk.recordmanager.server.facade.HarvestingFacade;
+import cz.mzk.recordmanager.server.facade.exception.JobExecutionFailure;
+import cz.mzk.recordmanager.server.model.HarvestFrequency;
+import cz.mzk.recordmanager.server.oai.dao.KrameriusConfigurationDAO;
+import cz.mzk.recordmanager.server.oai.dao.OAIHarvestConfigurationDAO;
+
+public class DailyScript implements Runnable {
+
+	private static Logger logger = LoggerFactory.getLogger("cz.mzk.recordmanager.server.automatization.DailyScript");
+
+	@Autowired
+	private HarvestingFacade harvestingFacade;
+
+	private OAIHarvestConfigurationDAO oaiHarvestConfigurationDAO;
+	
+	@Autowired
+	private KrameriusConfigurationDAO krameriusConfigurationDAO;
+
+	@Override
+	public void run() {
+		oaiHarvestConfigurationDAO.findAll().each { conf ->
+			if (conf.harvestFrequency == HarvestFrequency.DAILY) {
+				try {
+					harvestingFacade.incrementalHarvest(conf)
+				} catch (JobExecutionFailure jfe) {
+					logger.error(String.format("Incremental harvest of %s failed", conf), jfe);
+				}
+			}
+		}
+		krameriusConfigurationDAO.findAll().each { conf ->
+			if (conf.harvestFrequency == HarvestFrequency.DAILY) {
+				try {
+					harvestingFacade.incrementalHarvest(conf)
+				} catch (JobExecutionFailure jfe) {
+					logger.error(String.format("Incremental harvest of %s failed", conf), jfe);
+				}
+			}
+		}
+	}
+
+}
