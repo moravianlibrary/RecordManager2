6dcd46ab68eb56543a9aef1e7778214dacf2087e
Tomáš Cejpek <tomas.cejpek@email.cz>
2020-03-26 10:45:20 +0100

    add url kramerius-vs.mzk.cz for kram-mzk records

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/KramMzkMetadataMarcRecord.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/KramMzkMetadataMarcRecord.java
@@ -2,10 +2,17 @@ package cz.mzk.recordmanager.server.metadata.institutions;
 
 import cz.mzk.recordmanager.server.marc.MarcRecord;
 import cz.mzk.recordmanager.server.model.HarvestedRecord;
+import cz.mzk.recordmanager.server.oai.dao.FulltextKrameriusDAO;
+import cz.mzk.recordmanager.server.util.Constants;
+import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class KramMzkMetadataMarcRecord extends KramDefaultMetadataMarcRecord {
+
+	@Autowired
+	private FulltextKrameriusDAO fulltextKrameriusDAO;
 
 	public KramMzkMetadataMarcRecord(MarcRecord underlayingMarc, HarvestedRecord hr) {
 		super(underlayingMarc, hr);
@@ -13,7 +20,13 @@ public class KramMzkMetadataMarcRecord extends KramDefaultMetadataMarcRecord {
 
 	@Override
 	public List<String> getUrls() {
-		return generateUrl("http://www.digitalniknihovna.cz/mzk/uuid/");
+		List<String> results = new ArrayList<>();
+		String policy = fulltextKrameriusDAO.getPolicy(harvestedRecord.getId());
+
+		results.addAll(generateUrl("http://www.digitalniknihovna.cz/mzk/uuid/", policy));
+		if (Constants.DOCUMENT_AVAILABILITY_PROTECTED.equals(policy))
+			results.addAll(generateUrl("https://kramerius-vs.mzk.cz/view/", policy));
+		return results;
 	}
 
 }
