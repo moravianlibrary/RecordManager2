e074db91cd5a5c0374a41ecd778ee08b00d7b123
Tomáš Cejpek <tomas.cejpek@email.cz>
2020-04-17 14:47:38 +0200

    add reharvest of antikvariaty into facade

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/facade/ImportRecordFacade.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/facade/ImportRecordFacade.java
@@ -1,6 +1,7 @@
 package cz.mzk.recordmanager.server.facade;
 
 import java.io.File;
+import java.time.LocalDateTime;
 
 import cz.mzk.recordmanager.server.model.DownloadImportConfiguration;
 
@@ -17,5 +18,9 @@ public interface ImportRecordFacade {
 	void unpackAndImportRecordsJob(DownloadImportConfiguration dic);
 
 	void harvestInspirationsJob();
+
+	LocalDateTime getLastCompletedExecution(String jobName);
+
+	void reharvestAntikvariaty();
 
 }
