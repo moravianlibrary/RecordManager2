db2ffb91a0fd656ac9f04035134848cfc51fa4aa
Tomáš Cejpek <tomas.cejpek@email.cz>
2020-04-20 09:48:41 +0200

    update antikvariaty test

--- cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/imports/ImportAntikvariatyTest.java
+++ cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/imports/ImportAntikvariatyTest.java
@@ -33,68 +33,71 @@ public class ImportAntikvariatyTest extends AbstractTest {
 
 	@Autowired
 	private JobLauncher jobLauncher;
-	
+
 	@Autowired
-	private DownloadImportConfigurationDAO configDao;	
-	
-	@Autowired 
+	private DownloadImportConfigurationDAO configDao;
+
+	@Autowired
 	private AntikvariatyRecordDAO antikDao;
-	
+
 	@Autowired
 	private HttpClient httpClient;
-	
+
 	@BeforeMethod
 	public void initDb() throws Exception {
 		dbUnitHelper.init("dbunit/ImportRecords.xml");
 	}
-	
+
 	@Test
 	public void simpleTest() throws Exception {
-		
+
 		final Long confId = 500L;
 		DownloadImportConfiguration config = configDao.get(confId);
 		Assert.assertNotNull(config);
 		Assert.assertNotNull(config.getUrl());
-		
+
 		reset(httpClient);
 		InputStream response0 = this.getClass().getResourceAsStream("/antikvariaty/antikvariaty-base.xml");
 		expect(httpClient.executeGet("http://local.antikvariaty.mzk/file.xml")).andReturn(response0);
 		replay(httpClient);
-		
+
 		Job job = jobRegistry.getJob(Constants.JOB_ID_IMPORT_ANTIKVARIATY);
-		Map<String, JobParameter> params = new HashMap<String, JobParameter>();
+		Map<String, JobParameter> params = new HashMap<>();
 		params.put(Constants.JOB_PARAM_CONF_ID, new JobParameter(confId));
 		JobParameters jobParams = new JobParameters(params);
 		jobLauncher.run(job, jobParams);
-		
-		
+
+
 		AntikvariatyRecord toBeChangedRecord = antikDao.get(1L);
 		Assert.assertNotNull(toBeChangedRecord);
-		Assert.assertTrue(toBeChangedRecord.getCatalogueIds().size() == 1);
-		Assert.assertEquals(toBeChangedRecord.getCatalogueIds().get(0),"zb00026");
+		Assert.assertEquals(toBeChangedRecord.getCatalogueIds().size(), 1);
+		Assert.assertEquals(toBeChangedRecord.getCatalogueIds().get(0), "zb00026");
 		Assert.assertNotNull(toBeChangedRecord.getUpdated());
 		Assert.assertNotNull(toBeChangedRecord.getTitle());
 		Assert.assertNotNull(toBeChangedRecord.getUrl());
 		Assert.assertNotNull(toBeChangedRecord.getPublicationYear());
-		
-		
+		Assert.assertNotNull(toBeChangedRecord.getUpdatedOriginal());
+		Assert.assertNotNull(toBeChangedRecord.getLastHarvest());
+
 		reset(httpClient);
 		response0 = this.getClass().getResourceAsStream("/antikvariaty/antikvariaty-update.xml");
 		expect(httpClient.executeGet("http://local.antikvariaty.mzk/file.xml")).andReturn(response0);
 		replay(httpClient);
-		
+
 		job = jobRegistry.getJob(Constants.JOB_ID_IMPORT_ANTIKVARIATY);
-		params = new HashMap<String, JobParameter>();
+		params = new HashMap<>();
 		params.put(Constants.JOB_PARAM_CONF_ID, new JobParameter(confId));
 		params.put(Constants.JOB_PARAM_REPEAT, new JobParameter(1L));
 		jobParams = new JobParameters(params);
 		jobLauncher.run(job, jobParams);
-		
+
 		AntikvariatyRecord updatedRecord = antikDao.get(1L);
 		Assert.assertNotNull(updatedRecord);
-		Assert.assertTrue(updatedRecord.getCatalogueIds().size() == 1);
-		Assert.assertEquals(updatedRecord.getCatalogueIds().get(0),"xxxxxxxxxxxxxxx");
+		Assert.assertEquals(updatedRecord.getCatalogueIds().size(), 1);
+		Assert.assertEquals(updatedRecord.getCatalogueIds().get(0), "xxxxxxxxxxxxxxx");
 		Assert.assertTrue(updatedRecord.getUpdated().compareTo(toBeChangedRecord.getUpdated()) > 0);
+		Assert.assertTrue(updatedRecord.getUpdatedOriginal().compareTo(toBeChangedRecord.getUpdatedOriginal()) > 0);
+		Assert.assertTrue(updatedRecord.getLastHarvest().compareTo(toBeChangedRecord.getLastHarvest()) > 0);
 	}
 
 }
