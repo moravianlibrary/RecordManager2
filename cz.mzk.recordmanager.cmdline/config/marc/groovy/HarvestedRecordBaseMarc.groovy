157f255fe19bb01055457f86ab9b5af40744c9dc
Tomáš Cejpek <tomas.cejpek@email.cz>
2020-09-11 13:22:09 +0200

    add aleph adm_id

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/HarvestedRecordBaseMarc.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/HarvestedRecordBaseMarc.groovy
@@ -61,6 +61,7 @@ semantic_enrichment_txt_mv = getSemanticEnrichment()
 auto_conspectus_txt_mv = getAutoConspectus()
 periodical_availability_int_mv = getPeriodicalAvailability()
 sigla_display = getSigla()
+aleph_adm_id_txt_mv = getAlephAdmId()
 
 _hidden_isbn_annotation_obalkyknih = getFields "020a:902a"
 
