670e54d9498b41bece8f55c93b6734573e849dbe
Tomáš Cejpek <tomas.cejpek@email.cz>
2020-10-01 13:48:37 +0200

    add backlink for HISTOGRAFBIB

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/util/Constants.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/util/Constants.java
@@ -108,12 +108,14 @@ public class Constants {
 	public static final String PREFIX_AGROVOC			= "agrovoc";
 	public static final String PREFIX_ANL				= "anl";
 	public static final String PREFIX_AUTH				= "auth";
+	public static final String PREFIX_ARCHBIB			= "archbib";
 	public static final String PREFIX_BMC				= "bmc";
 	public static final String PREFIX_BOOKPORT			= "bookport";
 	public static final String PREFIX_CASLIN			= "caslin";
 	public static final String PREFIX_CBVK				= "cbvk";
 	public static final String PREFIX_CELITEBIB			= "celitebib";
 	public static final String PREFIX_CMUZ				= "cmuz";
+	public static final String PREFIX_CZHISTBIB			= "czhistbib";
 	public static final String PREFIX_DIVABIB			= "divabib";
 	public static final String PREFIX_FREE				= "free";
 	public static final String PREFIX_IIR				= "iir";
