1d3969df2b9df553b035ca390c988381d6f69484
Tomáš Cejpek <tomas.cejpek@email.cz>
2021-02-09 11:16:08 +0100

    filter for full set

---
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/miscellaneous/MappedSourceJobParametersValidator.java
@@ -0,0 +1,29 @@
+package cz.mzk.recordmanager.server.miscellaneous;
+
+import cz.mzk.recordmanager.server.springbatch.DefaultJobParametersValidator;
+import cz.mzk.recordmanager.server.springbatch.JobParameterDeclaration;
+import cz.mzk.recordmanager.server.util.Constants;
+import org.springframework.batch.core.JobParameter;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.JobParametersInvalidException;
+
+import java.util.Collection;
+import java.util.Collections;
+
+import static cz.mzk.recordmanager.server.springbatch.JobParameterDeclaration.param;
+
+public class MappedSourceJobParametersValidator extends DefaultJobParametersValidator {
+
+	@Override
+	public void validate(JobParameters parameters) throws JobParametersInvalidException {
+		super.validate(parameters);
+	}
+
+	@Override
+	public Collection<JobParameterDeclaration> getParameters() {
+		return Collections.singletonList(
+				param(Constants.JOB_PARAM_CONF_ID, JobParameter.ParameterType.LONG, true)
+		);
+	}
+
+}
