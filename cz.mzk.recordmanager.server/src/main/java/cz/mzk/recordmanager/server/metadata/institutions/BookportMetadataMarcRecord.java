a0546692d1290dcb724a6bf78eb1d260310435ee
Tomáš Cejpek <tomas.cejpek@email.cz>
2022-02-03 15:49:11 +0100

    update bookport url

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/BookportMetadataMarcRecord.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/BookportMetadataMarcRecord.java
@@ -8,11 +8,23 @@ import cz.mzk.recordmanager.server.util.SolrUtils;
 import org.marc4j.marc.DataField;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class BookportMetadataMarcRecord extends EbooksMetadataMarcRecord {
 
 	private static final String COMMENT = "Registrovaní uživatelé knihovny získají knihu po přihlášení přes eduID.cz na Bookportu";
+
+	private static final Pattern URL_PATTERN = Pattern.compile("(.*)(/kniha/.*)");
+
+	private static final Map<String, String> URL_MAP = new HashMap<>();
+
+	static {
+		URL_MAP.put(Constants.PREFIX_SVKUL, "/AccountSaml/SignIn/?idp=https%3A%2F%2Fsvkul.cz%2Fidp%2Fshibboleth&returnUrl=");
+	}
 
 	public BookportMetadataMarcRecord(MarcRecord underlayingMarc, HarvestedRecord hr) {
 		super(underlayingMarc, hr);
@@ -24,10 +36,19 @@ public class BookportMetadataMarcRecord extends EbooksMetadataMarcRecord {
 		for (DataField df : underlayingMarc.getDataFields("856")) {
 			if (df.getSubfield('u') != null) {
 				results.add(MetadataUtils.generateUrl(harvestedRecord.getHarvestedFrom().getLibrary().getName().toLowerCase(),
-						Constants.DOCUMENT_AVAILABILITY_MEMBER, df.getSubfield('u').getData(), COMMENT));
+						Constants.DOCUMENT_AVAILABILITY_MEMBER, parseUrl(df.getSubfield('u').getData()), COMMENT));
 			}
 		}
 		return results;
+	}
+
+	private String parseUrl(String f856u) {
+		Matcher matcher = URL_PATTERN.matcher(f856u);
+		String prefix = harvestedRecord.getHarvestedFrom().getLibrary().getName().toLowerCase();
+		if (matcher.matches() && URL_MAP.containsKey(prefix)) {
+			return matcher.group(1) + URL_MAP.get(prefix) + matcher.group(2);
+		}
+		return f856u;
 	}
 
 	@Override
