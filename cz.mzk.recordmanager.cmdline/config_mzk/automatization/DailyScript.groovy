131d920c747734fbf089543adf31a4369d38c632
Tomáš Cejpek <tomas.cejpek@email.cz>
2022-03-21 13:02:33 +0100

    update mzk scripts

--- cz.mzk.recordmanager.cmdline/config_mzk/automatization/DailyScript.groovy
+++ cz.mzk.recordmanager.cmdline/config_mzk/automatization/DailyScript.groovy
@@ -1,14 +1,11 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import java.time.LocalDateTime;
-
-import cz.mzk.recordmanager.server.facade.HarvestingFacade;
-import cz.mzk.recordmanager.server.facade.HarvestingFacadeImpl;
-import cz.mzk.recordmanager.server.facade.IndexingFacade;
-import cz.mzk.recordmanager.server.facade.DedupFacade;
-import cz.mzk.recordmanager.server.model.HarvestFrequency;
-import cz.mzk.recordmanager.server.oai.dao.OAIHarvestConfigurationDAO;
+import cz.mzk.recordmanager.server.facade.HarvestingFacade
+import cz.mzk.recordmanager.server.facade.IndexingFacade
+import cz.mzk.recordmanager.server.facade.exception.JobExecutionFailure
+import cz.mzk.recordmanager.server.model.HarvestFrequency
+import cz.mzk.recordmanager.server.oai.dao.OAIHarvestConfigurationDAO
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 
 public class DailyScript implements Runnable {
 
@@ -21,25 +18,17 @@ public class DailyScript implements Runnable {
 	private IndexingFacade indexingFacade;
 
 	@Autowired
-	private DedupFacade dedupFacade;
-
-	@Autowired
 	private OAIHarvestConfigurationDAO oaiHarvestConfigurationDAO;
 
 	@Override
 	public void run() {
-		// Obalky knih harvest (new table of content is published every 3rd day of the month)
-		LocalDateTime now = LocalDateTime.now();
-		if (now.getDayOfMonth() > 3) {
-			LocalDateTime lastHarvest = harvestingFacade.getLastObalkyKnihHarvest();
-			if (lastHarvest == null || lastHarvest.getMonth() != now.getMonth()) {
-				harvestingFacade.obalkyKnihHarvest();
-			}
-		}
-
 		oaiHarvestConfigurationDAO.findAll().each { conf ->
 			if (conf.harvestFrequency == HarvestFrequency.DAILY) {
-				harvestingFacade.incrementalHarvest(conf)
+				try {
+					harvestingFacade.incrementalHarvest(conf)
+				} catch (JobExecutionFailure jfe) {
+					logger.error(String.format("Incremental harvest of %s failed", conf), jfe);
+				}
 			}
 		}
 		indexingFacade.indexHarvestedRecords();
--- cz.mzk.recordmanager.cmdline/config_mzk/automatization/WeeklyScript.groovy
+++ cz.mzk.recordmanager.cmdline/config_mzk/automatization/WeeklyScript.groovy
@@ -1,17 +1,11 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import cz.mzk.recordmanager.server.facade.HarvestingFacade
+import cz.mzk.recordmanager.server.facade.exception.JobExecutionFailure
+import cz.mzk.recordmanager.server.oai.dao.OAIHarvestConfigurationDAO
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.beans.factory.annotation.Autowired
 
-import java.io.File;
-import java.time.LocalDateTime;
-
-import cz.mzk.recordmanager.server.facade.HarvestingFacade;
-import cz.mzk.recordmanager.server.facade.HarvestingFacadeImpl;
-import cz.mzk.recordmanager.server.facade.IndexingFacade;
-import cz.mzk.recordmanager.server.facade.DedupFacade;
-import cz.mzk.recordmanager.server.facade.ImportRecordFacade;
-import cz.mzk.recordmanager.server.model.HarvestFrequency;
-import cz.mzk.recordmanager.server.oai.dao.OAIHarvestConfigurationDAO;
+import java.time.LocalDateTime
 
 public class WeeklyScript implements Runnable {
 
@@ -21,19 +15,31 @@ public class WeeklyScript implements Runnable {
 	private HarvestingFacade harvestingFacade;
 
 	@Autowired
-	private IndexingFacade indexingFacade;
-
-	@Autowired
 	private OAIHarvestConfigurationDAO oaiHarvestConfigurationDAO;
 
 	@Override
 	public void run() {
 		oaiHarvestConfigurationDAO.findAll().each { conf ->
 			if (conf.url == "http://aleph.mzk.cz/OAI") {
-				harvestingFacade.fullHarvest(conf);
+				try {
+					harvestingFacade.fullHarvest(conf);
+				} catch (JobExecutionFailure jfe) {
+					logger.error(String.format("Full harvest of %s failed", conf), jfe);
+				}
 			}
 		}
-		indexingFacade.reindexHarvestedRecords();
+		// Obalky knih harvest (new table of content is published every 3rd day of the month)
+		LocalDateTime now = LocalDateTime.now();
+		if (now.getDayOfMonth() > 3) {
+			LocalDateTime lastHarvest = harvestingFacade.getLastObalkyKnihHarvest();
+			if (lastHarvest == null || lastHarvest.getMonth() != now.getMonth()) {
+				try {
+					harvestingFacade.obalkyKnihHarvest();
+				} catch (JobExecutionFailure jfe) {
+					logger.error(String.format("Harvest of obalkyknihToc failed"), jfe);
+				}
+			}
+		}
 	}
 
 }
