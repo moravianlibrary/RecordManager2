38ddc3ab330c83317adffe59ab19b0eb0b149065
Tomáš Cejpek <tomas.cejpek@email.cz>
2022-04-04 15:22:07 +0200

    update loc source

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/MarcitLocMetadataMarcRecord.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/MarcitLocMetadataMarcRecord.java
@@ -1,10 +1,9 @@
 package cz.mzk.recordmanager.server.metadata.institutions;
 
 import cz.mzk.recordmanager.server.marc.MarcRecord;
-import cz.mzk.recordmanager.server.metadata.MetadataMarcRecord;
 import cz.mzk.recordmanager.server.model.HarvestedRecord;
 
-public class MarcitLocMetadataMarcRecord extends MetadataMarcRecord {
+public class MarcitLocMetadataMarcRecord extends MarcitMetadataMarcRecord {
 
 	public MarcitLocMetadataMarcRecord(MarcRecord underlayingMarc, HarvestedRecord hr) {
 		super(underlayingMarc, hr);
--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/MarcitMetadataMarcRecord.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/MarcitMetadataMarcRecord.java
@@ -16,16 +16,14 @@ public class MarcitMetadataMarcRecord extends MetadataMarcRecord {
 		super(underlayingMarc, hr);
 	}
 
-	private static final Pattern LOC_ID = Pattern.compile("[-\\s[0-9]]*");
+	private static final Pattern LOC_ID = Pattern.compile("[-\\s0-9]*");
 
 	@Override
 	public List<Loc> getLocIds() {
 		List<Loc> results = new ArrayList<>();
 		for (String id : underlayingMarc.getFields("010", 'a')) {
-			System.out.println(id);
 			if (LOC_ID.matcher(id).matches()) {
 				results.add(Loc.create(CleaningUtils.replaceAll(id, Pattern.compile("[-\\s]"), "")));
-				System.out.println(Loc.create(CleaningUtils.replaceAll(id, Pattern.compile("[-\\s]"), "")));
 			}
 		}
 		return results;
