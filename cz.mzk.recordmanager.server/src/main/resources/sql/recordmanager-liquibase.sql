c6ef5999bd97c9d9ed4d6d64a0ed204f92b542b8
Tomáš Cejpek <tomas.cejpek@email.cz>
2022-07-21 11:50:19 +0200

    add MKJIHLAVA configuration

--- cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-liquibase.sql
+++ cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-liquibase.sql
@@ -2950,3 +2950,8 @@ UPDATE download_import_conf SET url='http://ereading.cz/xml/xml_rent.xml',import
 
 --changeset tomascejpek:340 context:cpk
 UPDATE oai_harvest_conf SET url='https://nacr.kpsys.cz/api/oai' WHERE import_conf_id=367;
+
+--changeset tomascejpek:341 context:cpk
+INSERT INTO library (id, name, url, catalog_url, city, region) VALUES (259, 'MKJIHLAVA', 'https://jihlava.tritius.cz/tritius/oai-provider', 'https://jihlava.tritius.cz/', 'Jihlava', 'VY');
+INSERT INTO import_conf (id, library_id, contact_person_id, id_prefix, base_weight, cluster_id_enabled, filtering_enabled, interception_enabled, is_library, harvest_frequency, item_id, mappings996) VALUES (459, 259, 200, 'mkjihlava', 11, false, true, true, true, 'U', 'other', 'tritius');
+INSERT INTO oai_harvest_conf (import_conf_id,url,set_spec,metadata_prefix,granularity) VALUES (459,'https://jihlava.tritius.cz/tritius/oai-provider','CPK_1','marc21',NULL);
