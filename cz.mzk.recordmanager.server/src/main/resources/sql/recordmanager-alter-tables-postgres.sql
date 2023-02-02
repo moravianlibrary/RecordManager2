f4a70b24555a29fdb11b89f9d5703d167d7ca250
Tomáš Cejpek <tomas.cejpek@email.cz>
2023-02-02 09:54:14 +0100

    update MZK config

--- cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-alter-tables-postgres.sql
+++ cz.mzk.recordmanager.server/src/main/resources/sql/recordmanager-alter-tables-postgres.sql
@@ -2827,3 +2827,6 @@ INSERT INTO oai_harvest_conf (import_conf_id,url,set_spec,metadata_prefix,granul
 INSERT INTO library (id, name, url, catalog_url, city, region) VALUES (99049, 'KRAM-ROZHLAS', '', '', '','kramerius');
 INSERT INTO import_conf (id,library_id,contact_person_id,id_prefix,base_weight,cluster_id_enabled,filtering_enabled,interception_enabled,is_library,harvest_frequency,mapping_script,generate_dedup_keys,mapping_dedup_script,item_id) VALUES (99049,99049,200,'kram-rozhlas',8,false,true,false,false,'U',null,true,null,null);
 INSERT INTO kramerius_conf (import_conf_id,url,url_solr,query_rows,metadata_stream,auth_token,fulltext_harvest_type,download_private_fulltexts,harvest_job_name,collection,availability_source_url,availability_dest_url) VALUES (99049,'https://kramerius.rozhlas.cz/search/api/v5.0',null,50,'BIBLIO_MODS',null,'solr',true,'krameriusHarvestJob',null,null,'https://kramerius.rozhlas.cz/uuid/');
+
+-- 02. 02. 2022 tomascejpek
+UPDATE import_conf SET mapping_script='LocalMzk.groovy,HarvestedRecordBaseMarc.groovy' WHERE id in (300,320,324);
