43bbdbbe9d7f604401fa5d5633652e38f6fc2b7b
Tomáš Cejpek <tomas.cejpek@email.cz>
2023-05-25 10:45:14 +0200

    fix test

--- cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/kramerius/fulltext/KrameriusFulltexterSolrTest.java
+++ cz.mzk.recordmanager.server/src/test/java/cz/mzk/recordmanager/server/kramerius/fulltext/KrameriusFulltexterSolrTest.java
@@ -1,13 +1,10 @@
 package cz.mzk.recordmanager.server.kramerius.fulltext;
 
-import static org.easymock.EasyMock.and;
-import static org.easymock.EasyMock.anyObject;
-import static org.easymock.EasyMock.capture;
-import static org.easymock.EasyMock.expect;
-import static org.easymock.EasyMock.replay;
-
-import java.util.List;
-
+import cz.mzk.recordmanager.server.AbstractTest;
+import cz.mzk.recordmanager.server.kramerius.ApiMappingFactory;
+import cz.mzk.recordmanager.server.kramerius.harvest.KrameriusHarvesterParams;
+import cz.mzk.recordmanager.server.model.FulltextKramerius;
+import cz.mzk.recordmanager.server.solr.SolrServerFacade;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
@@ -15,21 +12,27 @@ import org.apache.solr.common.SolrDocumentList;
 import org.apache.solr.common.util.NamedList;
 import org.easymock.Capture;
 import org.easymock.EasyMock;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import cz.mzk.recordmanager.server.AbstractTest;
-import cz.mzk.recordmanager.server.model.FulltextKramerius;
-import cz.mzk.recordmanager.server.solr.SolrServerFacade;
+import java.util.List;
+
+import static org.easymock.EasyMock.*;
 
 public class KrameriusFulltexterSolrTest extends AbstractTest {
 
 	private SolrServerFacade mockedSolrServer = EasyMock.createMock(SolrServerFacade.class);
 
+	@Autowired
+	private ApiMappingFactory apiMappingFactory;
+
 	@Test
 	public void test() throws Exception {
 		prepare();
-		KrameriusFulltexterSolr solr = new KrameriusFulltexterSolr(mockedSolrServer);
+		KrameriusHarvesterParams parameters = new KrameriusHarvesterParams();
+		parameters.setApiMapping(apiMappingFactory.getMapping("5"));
+		KrameriusFulltexterSolr solr = new KrameriusFulltexterSolr(mockedSolrServer, parameters);
 		List<FulltextKramerius> pages = solr.getFulltextObjects("uuid:111ad136-1f0a-4cd0-9d00-242155955bdc");
 		Assert.assertEquals(pages.size(), 3);
 	}
