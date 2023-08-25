d6282e035219ee729e80e350409cb1a340e55347
Tomáš Cejpek <tomas.cejpek@email.cz>
2023-08-25 10:53:07 +0200

    sort fields in local mzk records

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/LocalMzk.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/LocalMzk.groovy
@@ -5,3 +5,8 @@ local_acq_date = getMZKAcquisitionDateStamp()
 local_callnumber_str_mv = getFieldsUnique "996c"
 local_location_txt_mv = getFieldsUnique "996h"
 local_barcode_txt_mv = getFieldsUnique "996b"
+
+// sort
+author_sort_cz = getAuthorForSorting("100abcd:110abcd:111abcd:700abcd:710abcd:711abcd")
+title_sort_cz = getSortableTitle()
+publishDate_sort = publishDateSort = getPublishDateForSorting()
