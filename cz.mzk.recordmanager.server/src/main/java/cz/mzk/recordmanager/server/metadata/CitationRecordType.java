7920c01cc2cff292a73766ee0b32f7b0b33e9844
Tomáš Cejpek <tomas.cejpek@email.cz>
2023-09-08 12:58:23 +0200

    update citation formats

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/CitationRecordType.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/CitationRecordType.java
@@ -2,9 +2,12 @@ package cz.mzk.recordmanager.server.metadata;
 
 public enum CitationRecordType {
 	ACADEMIC_WORK("DS"),
-	MANUSCRIPTS("ND"), 
+	ELECTRONIC_ACADEMIC_WORK("eDS"),
+	MANUSCRIPTS("ND"),
 	ELECTRONIC_BOOK("eBK"),
-	BOOK("BK"), 
+	BOOK("BK"),
+	BOOK_PART("PB"),
+	ELECTRONIC_BOOK_PART("ePB"),
 	ELECTRONIC_PERIODICAL("eSE"),
 	PERIODICAL("SE"),
 	ELECTRONIC_CONTRIBUTION_PROCEEDINGS("ePC"),
@@ -16,6 +19,8 @@ public enum CitationRecordType {
 	NORMS("NO"),
 	LAW("LE"),
 	PATENT("PA"),
+	PROCEEDINGS("PR"),
+	ELECTRONIC_PROCEEDINGS("ePR"),
 	ERROR("error");
 	
 	private String format;
