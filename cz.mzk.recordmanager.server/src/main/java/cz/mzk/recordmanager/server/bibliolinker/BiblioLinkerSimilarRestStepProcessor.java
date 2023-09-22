447f38b23ac2f40ce1243869b9f357374284f07d
Tomáš Cejpek <tomas.cejpek@email.cz>
2023-09-22 14:10:44 +0200

    update rest step

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/bibliolinker/BiblioLinkerSimilarRestStepProcessor.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/bibliolinker/BiblioLinkerSimilarRestStepProcessor.java
@@ -6,7 +6,6 @@ import cz.mzk.recordmanager.server.model.HarvestedRecord;
 import cz.mzk.recordmanager.server.oai.dao.BiblioLinkerSimilarDAO;
 import cz.mzk.recordmanager.server.oai.dao.HarvestedRecordDAO;
 import cz.mzk.recordmanager.server.util.ProgressLogger;
-import org.hibernate.query.criteria.internal.expression.function.AggregationFunction;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
@@ -16,7 +15,7 @@ import org.springframework.stereotype.Component;
 import java.util.*;
 
 /**
- * Generic implementation of of ItemProcessor
+ * Generic implementation of ItemProcessor
  */
 @Component
 public class BiblioLinkerSimilarRestStepProcessor extends BiblioLinkerSimilarSimpleStepProcessor implements
@@ -28,12 +27,12 @@ public class BiblioLinkerSimilarRestStepProcessor extends BiblioLinkerSimilarSim
 	@Autowired
 	private BiblioLinkerSimilarDAO blSimilarDao;
 
-	private static Logger logger = LoggerFactory.getLogger(BiblioLinkerSimilarRestStepProcessor.class);
+	private static final Logger logger = LoggerFactory.getLogger(BiblioLinkerSimilarRestStepProcessor.class);
 
-	private ProgressLogger progressLogger = new ProgressLogger(logger, 1000);
+	private final ProgressLogger progressLogger = new ProgressLogger(logger, 1000);
 
 	// maximum similarities count per record
-	protected static final int MAX_SIMILARS = 5;
+	protected static final int MAX_SIMILAR_RECORDS = 5;
 
 	public BiblioLinkerSimilarRestStepProcessor() {
 	}
@@ -47,21 +46,40 @@ public class BiblioLinkerSimilarRestStepProcessor extends BiblioLinkerSimilarSim
 		Set<HarvestedRecord> toUpdate = new HashSet<>();
 		for (Long blId : biblioIdsList) {
 			Collection<HarvestedRecord> hrs = harvestedRecordDao.getByBiblioLinkerIdAndSimilarFlag(blId);
-			Collection<BiblioLinkerSimilar> bls = blSimilarDao.getByBilioLinkerId(blId, MAX_SIMILARS);
-			if (bls.isEmpty()) continue;
+			Map<Long, BiblioLinkerSimilar> potentialSimilar = getPotentialSimilar(blId);
+			if (potentialSimilar.isEmpty()) continue;
 			for (HarvestedRecord hr : hrs) {
 				if (hr.getDeleted() != null) continue;
-				Set<BiblioLinkerSimilar> similarIds = new TreeSet<>(hr.getBiblioLinkerSimilarUrls());
-				for (BiblioLinkerSimilar bl : bls) {
-					if (similarIds.size() >= MAX_SIMILARS) break;
-					similarIds.add(BiblioLinkerSimilar.create(bl.getUrlId(), bl.getHarvestedRecordSimilarId(), type));
+				Set<BiblioLinkerSimilar> actualSimilar = new TreeSet<>(hr.getBiblioLinkerSimilarUrls());
+				List<Long> similarIds = getBiblioLinkerIds(actualSimilar);
+				for (Map.Entry<Long, BiblioLinkerSimilar> entry : potentialSimilar.entrySet()) {
+					if (actualSimilar.size() >= MAX_SIMILAR_RECORDS) break;
+					if (similarIds.contains(entry.getKey())) continue;
+					actualSimilar.add(BiblioLinkerSimilar.create(entry.getValue().getUrlId(), entry.getValue().getHarvestedRecordSimilarId(), type));
+					similarIds.add(entry.getKey());
 					toUpdate.add(hr);
 				}
-				hr.setBiblioLinkerSimilarUrls(new ArrayList<>(similarIds));
+				hr.setBiblioLinkerSimilarUrls(new ArrayList<>(actualSimilar));
 				progressLogger.incrementAndLogProgress();
 			}
 		}
 		return new ArrayList<>(toUpdate);
+	}
+
+	protected Map<Long, BiblioLinkerSimilar> getPotentialSimilar(Long blId) {
+		Map<Long, BiblioLinkerSimilar> results = new HashMap<>();
+		for (BiblioLinkerSimilar bs : blSimilarDao.getByBilioLinkerId(blId, MAX_SIMILAR_RECORDS * 2)) {
+			results.put(bs.getHarvestedRecordSimilar().getBiblioLinker().getId(), bs);
+		}
+		return results;
+	}
+
+	protected List<Long> getBiblioLinkerIds(Set<BiblioLinkerSimilar> similars) {
+		List<Long> results = new ArrayList<>();
+		for (BiblioLinkerSimilar bs : similars) {
+			results.add(bs.getHarvestedRecordSimilar().getBiblioLinker().getId());
+		}
+		return results;
 	}
 
 }
