f65a89f2e87d12ef3bdcb521cf8e3b681edae7f5
Tomáš Cejpek <tomas.cejpek@email.cz>
2024-09-13 12:07:50 +0200

    add solr field for previous owner

--- cz.mzk.recordmanager.cmdline/config/marc/groovy/LocalMzk.groovy
+++ cz.mzk.recordmanager.cmdline/config/marc/groovy/LocalMzk.groovy
@@ -7,6 +7,7 @@ callnumber_second_str_mv = getFields("996h").collect{it -> it.replace(' ', '|')}
 callnumber_search = getFieldsUnique "996c"
 local_location_txt_mv = getFieldsUnique "996h"
 local_barcode_txt_mv = getFieldsUnique "996b"
+previous_owner_txt_mv = getFieldsUnique "981ab:982ab:983ab"
 
 // sort
 author_sort_cz = getAuthorForSorting("100abcd:110abcd:111abcd:700abcd:710abcd:711abcd")
