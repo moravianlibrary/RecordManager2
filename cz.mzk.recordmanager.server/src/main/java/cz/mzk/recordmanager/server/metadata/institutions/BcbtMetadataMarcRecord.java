17de92e029a678ca2641cdc62abdd4093450f044
Tomáš Cejpek <tomas.cejpek@email.cz>
2025-07-30 16:08:21 +0200

    add links for BCBT

--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/BcbtMetadataMarcRecord.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/metadata/institutions/BcbtMetadataMarcRecord.java
@@ -2,9 +2,14 @@ package cz.mzk.recordmanager.server.metadata.institutions;
 
 import cz.mzk.recordmanager.server.marc.MarcRecord;
 import cz.mzk.recordmanager.server.metadata.MetadataMarcRecord;
+import cz.mzk.recordmanager.server.model.EVersionUrl;
 import cz.mzk.recordmanager.server.model.HarvestedRecord;
 import cz.mzk.recordmanager.server.model.HarvestedRecordFormat.HarvestedRecordFormatEnum;
+import cz.mzk.recordmanager.server.util.Constants;
+import cz.mzk.recordmanager.server.util.constants.EVersionConstants;
+import org.marc4j.marc.DataField;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
@@ -14,6 +19,8 @@ public class BcbtMetadataMarcRecord extends MetadataMarcRecord {
 	public BcbtMetadataMarcRecord(MarcRecord underlayingMarc, HarvestedRecord hr) {
 		super(underlayingMarc, hr);
 	}
+
+	private static final String LINK_BCBT = "https://knihoveda.lib.cas.cz/Record/%s";
 
 	private static final List<HarvestedRecordFormatEnum> AUDIO_VIDEO = Arrays.asList(
 			HarvestedRecordFormatEnum.AUDIO_CASSETTE,
@@ -35,6 +42,20 @@ public class BcbtMetadataMarcRecord extends MetadataMarcRecord {
 		List<HarvestedRecordFormatEnum> results = super.getDetectedFormatList();
 		if (!Collections.disjoint(results, AUDIO_VIDEO))
 			return Collections.singletonList(HarvestedRecordFormatEnum.BOOKS);
+		return results;
+	}
+
+	@Override
+	public List<String> getUrls() {
+		List<String> results = new ArrayList<>(super.getUrls());
+
+		if (underlayingMarc.getControlField("001") != null) {
+			EVersionUrl url = EVersionUrl.create(harvestedRecord.getHarvestedFrom().getIdPrefix(),
+					Constants.DOCUMENT_AVAILABILITY_UNKNOWN,
+					String.format(LINK_BCBT, underlayingMarc.getControlField("001")), EVersionConstants.BCBT_LINK);
+			if (url != null) results.add(url.toString());
+		}
+
 		return results;
 	}
 }
--- cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/util/constants/EVersionConstants.java
+++ cz.mzk.recordmanager.server/src/main/java/cz/mzk/recordmanager/server/util/constants/EVersionConstants.java
@@ -3,6 +3,7 @@ package cz.mzk.recordmanager.server.util.constants;
 public class EVersionConstants {
 
 	public static final String BMC_LINK = "záznam v BMČ";
+	public static final String BCBT_LINK = "záznam v Knihovědě";
 	public static final String BOOKPORT_LINK = "bookport_link";
 	public static final String CATALOG_EBOOK_LINK = "catalog_ebook_link";
 	public static final String CATALOG_SERIAL_LINK = "catalog_serial_link";
